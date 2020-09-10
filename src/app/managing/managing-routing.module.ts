9d0dfef68799f3691b6c684c23f9ba57decfb478
Alessandro Gaggia <alex@besharp.it>
2020-09-10 12:44:15 +0200

    moved start-screen in start module to divide logical feats

--- src/app/managing/managing-routing.module.ts
+++ src/app/managing/managing-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { StartScreenComponent } from './start-screen/start-screen.component';
+import { StartScreenComponent } from '../start/start-screen/start-screen.component';
 import { SetupFirstAccountComponent } from './setup-first-account/setup-first-account.component';
 import {NoAppbarLayoutComponent} from '../layout/noappbar-layout/noappbar-layout.component';
 import {CreateAccountComponent} from './create-account/create-account.component';
@@ -11,10 +11,6 @@ const routes: Routes = [
     path: '',
     component: NoAppbarLayoutComponent, // Used as a layout for the sessions
     children: [
-      {
-        path: 'start-screen',
-        component: StartScreenComponent // Used to check for eventual dependencies and anything necessary to make the app running; is also the place where you can put validation checks
-      },
       {
         path: 'setup-first-account',
         component: SetupFirstAccountComponent // Setup the first account

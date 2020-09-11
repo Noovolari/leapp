a05cfc5ef6e5694780e735c327917303c92649a9
Alessandro Gaggia <alex@besharp.it>
2020-09-11 19:04:42 +0200

    Added create account new graphics

--- src/app/services/provider-manager.service.ts
+++ src/app/services/provider-manager.service.ts
@@ -19,7 +19,6 @@ export class ProviderManagerService {
   selectedType;
   selectedAccount;
   selectedRole;
-  roles;
 
   /**
    * Used to manage all the choices done in the app regarding the correct provider to use:
@@ -65,10 +64,9 @@ export class ProviderManagerService {
    * @param selectedAccount - the selected account as a parent for
    * @param selectedRole - the selected role of the parent
    * @param selectedType - the type of AWS account, if any, that you have selected
-   * @param roles - the roles for AWS accounts
    * @param form - the form to use
    */
-  saveFirstAccount(accountId, accountType, selectedAccount, selectedRole, selectedType, roles, form) {
+  saveFirstAccount(accountId, accountType, selectedAccount, selectedRole, selectedType, form) {
     // Set our variable to avoid sending them to all methods;
     // besides the scope of this service is to manage saving and editing
     // of multi providers so having some helper class variables is ok
@@ -78,7 +76,6 @@ export class ProviderManagerService {
     this.selectedAccount = selectedAccount;
     this.selectedRole = selectedRole;
     this.form = form;
-    this.roles = roles;
 
     // Before we need to save the first workspace and call google: this is done only the first ime so it is not used in other classes
     // Now we get the default configuration to obtain the previously saved idp url
@@ -87,23 +84,20 @@ export class ProviderManagerService {
     // Update Configuration
     if (accountType === AccountType.AWS) {
       configuration.federationUrl = form.value.federationUrl;
+      this.configurationService.updateConfigurationFileSync(configuration);
+
+      // Set our response type
+      const responseType = IdpResponseType.SAML;
+
+      // When the token is received save it and go to the setup page for the first account
+      const sub = this.workspaceService.googleEmit.subscribe((googleToken) => this.ngZone.run(() => this.createNewWorkspace(googleToken, configuration.federationUrl, responseType)));
+
+      // Call the service for working on the first login event to the user idp
+      // We add the helper for account choosing just to be sure to give the possibility to call the correct user
+      this.workspaceService.getIdpTokenInSetup(form.value.federationUrl, responseType);
     } else {
-      configuration.federationUrl = '';
+      this.decideSavingMethodAndSave();
     }
-    this.configurationService.updateConfigurationFileSync(configuration);
-
-    // Set our response type
-    const responseType = IdpResponseType.SAML;
-
-    // When the token is received save it and go to the setup page for the first account
-    const sub = this.workspaceService.googleEmit.subscribe((googleToken) => this.ngZone.run(() => this.createNewWorkspace(googleToken, configuration.federationUrl, responseType)));
-
-    // Call the service for working on the first login event to the user idp
-    // We add the helper for account choosing just to be sure to give the possibility to call the correct user
-    this.workspaceService.getIdpTokenInSetup(form.value.federationUrl, responseType);
-
-    // Clear subscription when not needed
-
   }
 
   /**
@@ -113,10 +107,9 @@ export class ProviderManagerService {
    * @param selectedAccount - the selected account as a parent for
    * @param selectedRole - the selected role of the parent
    * @param selectedType - the type of AWS account, if any, that you have selected
-   * @param roles - the roles for AWS accounts
    * @param form - the form to use
    */
-  saveAccount(accountId, accountType, selectedAccount, selectedRole, selectedType, roles, form) {
+  saveAccount(accountId, accountType, selectedAccount, selectedRole, selectedType, form) {
     // Set our variable to avoid sending them to all methods;
     // besides the scope of this service is to manage saving and editing
     // of multi providers so having some helper class variables is ok
@@ -126,7 +119,6 @@ export class ProviderManagerService {
     this.selectedAccount = selectedAccount;
     this.selectedRole = selectedRole;
     this.form = form;
-    this.roles = roles;
     this.decideSavingMethodAndSave();
   }
 
@@ -169,7 +161,7 @@ export class ProviderManagerService {
    * Save azure account
    */
   saveAzureAccount() {
-    if (this.formValid(this.form, this.roles)) {
+    if (this.formValid(this.form, this.accountType, this.selectedType)) {
       try {
         // Try to create the truster account
         const created = this.azureAccountService.addAzureAccountToWorkSpace(
@@ -180,7 +172,7 @@ export class ProviderManagerService {
         this.sessionService.addSession(
           this.form.value.subscriptionId,
           null,
-          true);
+          false);
 
         return created;
       } catch (err) {
@@ -197,7 +189,7 @@ export class ProviderManagerService {
    * This will be removed after created the correct file also in normal mode
    */
   saveAwsTrusterAccount() {
-    if (this.formValid(this.form, this.roles)) {
+    if (this.formValid(this.form, this.accountType, this.selectedType)) {
       try {
         // Try to create the truster account
         const created = this.trusterAccountService.addTrusterAccountToWorkSpace(
@@ -205,9 +197,15 @@ export class ProviderManagerService {
           this.form.value.name,
           this.selectedAccount,
           this.selectedRole,
-          this.generateRolesFromNames(this.form.value.accountNumber),
+          this.generateRolesFromNames(this.form),
           this.form.value.idpArn,
           this.form.value.myRegion);
+
+        // When you create an account you also define a possible session: in this case, being the only one we default it to true
+        this.sessionService.addSession(
+          this.form.value.accountNumber,
+          this.generateRolesFromNames(this.form)[0].name,
+          false);
 
         return created;
       } catch (err) {
@@ -221,20 +219,20 @@ export class ProviderManagerService {
   }
 
   saveAwsFederatedAccount() {
-    if (this.formValid(this.form, this.roles)) {
+    if (this.formValid(this.form, this.accountType, this.selectedType)) {
       try {
         // Add a federation Account to the workspace
         const created = this.federatedAccountService.addFederatedAccountToWorkSpace(
           this.form.value.accountNumber,
           this.form.value.name,
-          this.generateRolesFromNames(this.form.value.accountNumber),
+          this.generateRolesFromNames(this.form),
           this.form.value.idpArn, this.form.value.myRegion);
 
         // When you create an account you also define a possible session: in this case, being the only one we default it to true
         this.sessionService.addSession(
           this.form.value.accountNumber,
-          this.generateRolesFromNames(this.form.value.accountNumber)[0].name,
-          true);
+          this.generateRolesFromNames(this.form)[0].name,
+          false);
 
         return created;
       } catch (err) {
@@ -252,53 +250,50 @@ export class ProviderManagerService {
    * In the future we will put this in a service to create validation factory:
    * this way depending on new accounts we jkust need to pass the form object to the validator
    */
-  formValid(form, roles) {
-    // First check the type of account we are creating
-    if (this.accountType === 'AWS') {
+  formValid(form, accountType, selectedType) {
 
-      // Both have roles check
-      const checkRoles = roles.length > 0;
+    // First check the type of account we are creating
+    if (accountType === AccountType.AWS) {
 
       // We are in AWS check if we are saving a Federated or a Truster
-      if (this.selectedType === 'federated') {
+      if (selectedType === 'federated') {
         // Check Federated fields
         const checkFields = form.controls['name'].valid &&
           form.controls['federationUrl'].valid &&
           form.controls['accountNumber'].valid &&
+          form.controls['role'].valid &&
           form.controls['idpArn'].valid;
 
-        return checkRoles && checkFields;
+        return checkFields;
       } else {
         // Check Truster fields
         const checkFields = form.controls['name'].valid &&
           form.controls['federationUrl'].valid &&
           form.controls['accountNumber'].valid &&
+          form.controls['role'].valid &&
           form.controls['federatedAccount'].valid &&
           form.controls['federatedRole'].valid;
 
-        return checkRoles && checkFields;
+        return checkFields;
       }
     } else {
       // Check Azure fields
       return form.controls['name'].valid &&
-        form.controls['subscriptionId'].valid;
+             form.controls['subscriptionId'].valid;
     }
     return false;
   }
 
   /**
    * By using the names we create the corresponding roles to be pushed inside the account configuration
-   * @param accountNumber - the account number we use to construct the role arn
+   * @param form - the form to access the role from
    * @returns - {any[]} - returns a list of aws roles
    */
-  generateRolesFromNames(accountNumber: string) {
+  generateRolesFromNames(form) {
     const awsRoles = [];
-    this.roles.forEach(role => {
-      awsRoles.push({
-        name: role,
-        roleArn: `arn:aws:iam::${accountNumber}:role/${role}`
-      });
-    });
+    const role = form.controls['role'].value;
+    const accountNumber = form.controls['accountNumber'].value;
+    awsRoles.push({ name: role, roleArn: `arn:aws:iam::${accountNumber}:role/${role}` });
     return awsRoles;
   }
 

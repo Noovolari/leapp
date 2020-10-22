4ca36b17cd8d827ee53b234e8e898e1edb66a9ac
Alessandro Gaggia <alex@besharp.it>
2020-10-22 19:02:23 +0200

    Removed error during ssm process

--- src/app/services/ssm.service.ts
+++ src/app/services/ssm.service.ts
@@ -5,8 +5,7 @@ import {forkJoin, Observable, of} from 'rxjs';
 import {catchError, map} from 'rxjs/internal/operators';
 import {AwsCredential} from '../models/credential';
 
-const SSM = require('aws-sdk/clients/ssm');
-const EC2 = require('aws-sdk/clients/ec2');
+const AWS = require('aws-sdk');
 
 @Injectable({
   providedIn: 'root'
@@ -29,8 +28,9 @@ export class SsmService {
    */
   setInfo(data: any, region): Observable<SsmResult> {
     // Set your SSM client and EC2 client
-    this.ssmClient = new SSM(this.setConfig(data, region));
-    this.ec2Client = new EC2(this.setConfig(data, region));
+    AWS.config.update(this.setConfig(data, region));
+    this.ssmClient = new AWS.SSM();
+    this.ec2Client = new AWS.EC2();
 
     // Fix for Ec2 clients from electron app
     this.app.setFilteringForEc2Calls();
@@ -45,94 +45,45 @@ export class SsmService {
   submit(): Observable<SsmResult> {
     const mythis = this;
     this.instances = [];
-    return forkJoin<any>(
-      [
-        new Observable(observer => {
+    return new Observable(observer => {
           this.ssmClient.describeInstanceInformation({}, (err, data) => {
             if (err) {
-              this.app.logger('Error in describe instances', LoggerLevel.ERROR, this, err.stack);
-              observer.error(err);
+              this.app.logger('You are not Authorized to perform SSM Describe Instance with your current credentials', LoggerLevel.ERROR, this, err.stack);
+              mythis.app.toast('You are not Authorized to perform SSM Describe Instance with your current credentials, please check the log files for more information.', ToastLevel.ERROR, 'SSM error.');
+              observer.error({ status: false, instances: mythis.instances });
             } else {
-              observer.next(data);
-            }
-            observer.complete();
-          });
-        }),
-        new Observable(observer => {
-          this.ec2Client.describeInstances({ MaxResults: 1000 }, (err, data) => {
-            if (err) {
-              this.app.logger('Error in describe instances', LoggerLevel.ERROR, this, err.stack);
-              observer.error(err);
-            } else {
-              observer.next(data);
-            }
-            observer.complete();
-          });
-        }).pipe(
-          catchError(err => {
-            this.app.logger('Generic Error in ssm starting', LoggerLevel.ERROR, this, err.stack);
-            return of(err);
-          })
-        )
-      ]
-    ).pipe(
-      map((data: any[]) => {
-        const dataSSM = data[0];
-        const dataEC2 = data[1];
-
-        // Once we have obtained data from SSM and EC2, we verify the list are not empty
-        if (dataSSM['InstanceInformationList'] && dataSSM['InstanceInformationList'].length > 0) {
-          // filter only the instances that are currently online
-          mythis.instances = dataSSM['InstanceInformationList'].filter(i => i.PingStatus === 'Online' );
-          if (mythis.instances.length > 0) {
-            // If there are and the Ec2 information contains reservation...
-            if (dataEC2['Reservations'] && dataEC2['Reservations'].length > 0) {
-              // For every instance that fullfill we obtain...
-              mythis.instances.forEach(instance => {
-                // Add name if exists
-                const instanceId = instance.InstanceId;
-                // Get the reservation of that instance
-                const reservation = dataEC2['Reservations'].filter(res => res.Instances[0].InstanceId === instanceId);
-                if (reservation) {
-                  // If there is, we complete getting the instance info
-                  const ec2Instance = reservation[0].Instances[0];
-                  // Get the instance tags
-                  const tags = ec2Instance.Tags;
-                  if (tags && tags.length > 0) {
-                    // We set the tag name value
-                    const tag = tags.filter(t => t.Key === 'Name');
-                    instance['Name'] = tag[0] ? tag[0].Value : instance.InstanceId;
-                  }
+              const dataSSM = data;
+              // Once we have obtained data from SSM and EC2, we verify the list are not empty
+              if (dataSSM['InstanceInformationList'] && dataSSM['InstanceInformationList'].length > 0) {
+                // filter only the instances that are currently online
+                mythis.instances = dataSSM['InstanceInformationList'].filter(i => i.PingStatus === 'Online' );
+                if (mythis.instances.length > 0) {
+                  // For every instance that fullfill we obtain...
+                  mythis.instances.forEach(instance => {
+                    // Add name if exists
+                    const instanceId = instance.InstanceId;
+                    instance['ComputerName'] = instance['ComputerName'] || instance.InstanceId;
+                    instance['Name'] = instance['ComputerName'];
+                  });
+                  // We have found and managed a list of instances
+                  this.app.logger('Obtained smm info from AWS for SSM', LoggerLevel.INFO, this);
+                  observer.next({ status: true, instances: mythis.instances });
                 } else {
-                  instance['Name'] = instance.InstanceId;
+                  // No instances usable
+                  mythis.app.logger('No instances are accessible by this Role.', LoggerLevel.WARN, this);
+                  mythis.app.toast('No instances are accessible by this Role.', ToastLevel.WARN, 'No instance for SSM.');
+                  observer.error({ status: false, instances: mythis.instances });
                 }
-              });
-
-            } else {
-              mythis.instances = mythis.instances.map(i => { i['Name'] = i.InstanceId; return i; });
+              } else {
+                // No instances usable
+                mythis.app.logger('No instances are accessible by this Role.', LoggerLevel.WARN, this);
+                mythis.app.toast('No instances are accessible by this Role.', ToastLevel.WARN, 'No instance for SSM.');
+                observer.error({ status: false, instances: mythis.instances });
+              }
             }
-            // We have found and managed a list of instances
-            return { status: true, instances: mythis.instances };
-          } else {
-            // No instances usable
-            mythis.app.logger('No instances are accessible by this Role.', LoggerLevel.WARN, this);
-            mythis.app.toast('No instances are accessible by this Role.', ToastLevel.WARN, 'No instance for SSM.');
-            return { status: false, instances: mythis.instances };
-          }
-        } else {
-          // No instances usable
-          mythis.app.logger('No instances are accessible by this Role.', LoggerLevel.WARN, this);
-          mythis.app.toast('No instances are accessible by this Role.', ToastLevel.WARN, 'No instance for SSM.');
-          return { status: false, instances: mythis.instances };
-        }
-      }),
-      catchError(err =>  {
-        // A problem occured
-        this.app.logger('You are not Authorized to perform SSM with your current credentials', LoggerLevel.ERROR, this, err.stack);
-        mythis.app.toast('You are not Authorized to perform this operation with your current credentials, please check the log files for more information.', ToastLevel.ERROR, 'SSM error.');
-        return of({ status: false, instances: mythis.instances });
-      })
-    );
+            observer.complete();
+          });
+    });
   }
 
   /**
@@ -155,17 +106,15 @@ export class SsmService {
   setConfig(data: AwsCredential , region) {
 
     return data.aws_session_token ? {
-      region,
-      credentials: {
+        region,
         accessKeyId: data.aws_access_key_id,
         secretAccessKey: data.aws_secret_access_key,
         sessionToken: data.aws_session_token
-      }} : {
-      region,
-      credentials: {
+      } : {
+        region,
         accessKeyId: data.aws_access_key_id,
         secretAccessKey: data.aws_secret_access_key
-      }};
+      };
   }
 }
 

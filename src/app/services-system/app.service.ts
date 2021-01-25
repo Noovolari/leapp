d2b00ab52c34920d7abfd640a5122fa00dfac618
Owen Valentine <owenvalentine@gmail.com>
2021-01-25 17:28:56 +0200

    Add extra STS endpoints to match region list

--- src/app/services-system/app.service.ts
+++ src/app/services-system/app.service.ts
@@ -23,7 +23,8 @@ export class AppService extends NativeService {
   newWin;
 
   stsEndpointsPerRegion = new Map([
-    ['us-east-1', 'https://sts.us-east-1.amazonaws.com'],
+    ['af-south-1', 'https://sts.af-south-1.amazonaws.com'],
+    ['ap-east-1', 'https://sts.ap-east-1.amazonaws.com'],
     ['ap-northeast-1', 'https://sts.ap-northeast-1.amazonaws.com'],
     ['ap-northeast-2', 'https://sts.ap-northeast-2.amazonaws.com'],
     ['ap-northeast-3', 'https://sts.ap-northeast-3.amazonaws.com'],
@@ -31,14 +32,20 @@ export class AppService extends NativeService {
     ['ap-southeast-1', 'https://sts.ap-southeast-1.amazonaws.com'],
     ['ap-southeast-2', 'https://sts.ap-southeast-2.amazonaws.com'],
     ['ca-central-1', 'https://sts.ca-central-1.amazonaws.com'],
+    ['cn-north-1', 'https://sts.cn-north-1.amazonaws.com.cn'],
+    ['cn-northwest-1', 'https://sts.cn-northwest-1.amazonaws.com.cn'],
     ['eu-central-1', 'https://sts.eu-central-1.amazonaws.com'],
     ['eu-north-1', 'https://sts.eu-north-1.amazonaws.com'],
     ['eu-south-1', 'https://sts.eu-south-1.amazonaws.com'],
     ['eu-west-1', 'https://sts.eu-west-1.amazonaws.com'],
     ['eu-west-2', 'https://sts.eu-west-2.amazonaws.com'],
     ['eu-west-3', 'https://sts.eu-west-3.amazonaws.com'],
+    ['me-south-1', 'https://sts.me-south-1.amazonaws.com'],
     ['sa-east-1', 'https://sts.sa-east-1.amazonaws.com'],
+    ['us-east-1', 'https://sts.us-east-1.amazonaws.com'],
     ['us-east-2', 'https://sts.us-east-2.amazonaws.com'],
+    ['us-gov-east-1', 'https://sts.us-gov-east-1.amazonaws.com'],
+    ['us-gov-west-1', 'https://sts.us-gov-west-1.amazonaws.com'],
     ['us-west-1', 'https://sts.us-west-1.amazonaws.com'],
     ['us-west-2', 'https://sts.us-west-2.amazonaws.com']
   ]);

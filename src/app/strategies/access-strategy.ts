cda5be45fa2422540907f7ba0719370864800592
Andrea Cavagna <andrea.cavagna@besharp.it>
2021-04-27 10:40:00 +0200

    refactored session.service.ts

--- src/app/strategies/access-strategy.ts
+++ src/app/strategies/access-strategy.ts
@@ -3,9 +3,10 @@ import {AccountType} from '../models/AccountType';
 import {CredentialsInfo} from '../models/credentials-info';
 
 export interface AccessStrategy {
+
+  createAccount(  accountName: string, type: AccountType, region: string, accountInfo: { [key: string]: any }): Account;
+
   generateCredentials(sessionId: string): CredentialsInfo;
   applyCredentials(credentialsInfo: CredentialsInfo): Promise<void>;
   deApplyCredentials(credentialsInfo: CredentialsInfo): Promise<void>;
-
-  createAccount(  accountName: string, type: AccountType, region: string, accountInfo: { [key: string]: any }): Account;
 }

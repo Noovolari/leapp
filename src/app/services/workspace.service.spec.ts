1f1c39d2390cd561d0893cab2dd262b6e96f5fe1
Andrea Cavagna <andrea.cavagna@besharp.it>
2021-04-27 16:54:41 +0200

    chore: fixed spec in workspace.service.spec.ts

--- src/app/services/workspace.service.spec.ts
+++ src/app/services/workspace.service.spec.ts
@@ -3,12 +3,12 @@ import {TestBed} from '@angular/core/testing';
 
 describe('WorkspaceService', () => {
   let workspaceService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({ providers: [WorkspaceService] });
+    TestBed.configureTestingModule({
+      providers: [WorkspaceService]
+    });
     workspaceService = TestBed.inject(WorkspaceService);
   });
-
   it('should be created', () => {
     expect(workspaceService).toBeTruthy();
   });

cbd334714770cd42371aacaac2cb9b6f3d86a7fb
Andrea Cavagna <andrea.cavagna@besharp.it>
2021-04-27 10:40:00 +0200

    refactored session.service.ts

--- src/app/strategies/aws-plain-strategy.spec.ts
+++
@@ -1,8 +0,0 @@
-import {AwsPlainStrategy} from './aws-plain-strategy';
-
-
-describe('AwsPlainStrategy', () => {
-  it('should create an instance', () => {
-    expect(new AwsPlainStrategy()).toBeTruthy();
-  });
-});

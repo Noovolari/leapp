caec82d5e174e1a48e5c288b18768d127826caf3
Andrea Cavagna <andrea.cavagna@besharp.it>
2021-04-27 11:08:42 +0200

    added workspace class for definition and constructor

--- src/app/models/workspace.ts
+++ src/app/models/workspace.ts
@@ -1,27 +1,29 @@
 import {Session} from './session';
+import * as uuid from 'uuid';
+import {environment} from '../../environments/environment';
 
-export interface Workspace {
-  type?: string;
+export class Workspace {
 
-  // TODO WHY there are more than one workspace
-  name: string;
-  lastIDPToken?: string;
-  setupDone?: boolean;
-  idpUrl?: { id: string, url: string }[];
-  profiles?: { id: string, name: string }[];
+  idpUrl: { id: string, url: string }[];
+  profiles: { id: string, name: string }[];
+  sessions: Session[];
 
   proxyConfiguration: {
-    proxyProtocol?: string;
-    proxyUrl?: string;
-    proxyPort?: string;
-    username?: string;
-    password?: string;
+    proxyProtocol: string;
+    proxyUrl: string;
+    proxyPort: string;
+    username: string;
+    password: string;
   };
 
-  azureConfig: string;
-  azureProfile: string;
-
-  sessions: Session[];
   defaultRegion: string;
   defaultLocation: string;
+
+  constructor() {
+    this.idpUrl = [];
+    this.profiles = [{ id: uuid.v4(), name: 'default'}, {id: uuid.v4(), name: 'default-azure'}];
+    this.sessions = [];
+    this.defaultRegion = environment.defaultRegion;
+    this.defaultLocation = environment.defaultLocation;
+  }
 }

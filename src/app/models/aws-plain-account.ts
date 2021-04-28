dfb91c1665ea02e396fbbbd60eb9c960276e8969
Andrea Cavagna <andrea.cavagna@besharp.it>
2021-04-28 17:49:25 +0200

    refactor: removed profileId and set type by default in account creation

--- src/app/models/aws-plain-account.ts
+++ src/app/models/aws-plain-account.ts
@@ -2,21 +2,19 @@ import {AccountType} from './AccountType';
 import {Account} from './account';
 
 export class AwsPlainAccount extends Account {
-  accountId: string;
   accountName: string;
   accessKey: string;
   secretKey: string;
   region: string;
   mfaDevice?: string;
-  profileId: string;
   type: AccountType;
 
-  constructor(accountName: string, type: AccountType, region: string, accessKey: string, secretKey: string, profileId: string, mfaDevice?: string) {
-    super(accountName, type, region );
+  constructor(accountName: string, region: string, accessKey: string, secretKey: string, mfaDevice?: string) {
+    super(accountName, region);
     this.accessKey = accessKey;
     this.secretKey = secretKey;
     this.mfaDevice = mfaDevice;
-    this.profileId = profileId;
+    this.type = AccountType.AWS_PLAIN_USER;
   }
 
 }

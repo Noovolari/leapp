5a47952bb01aa9d3895c8f75baff92d10253728f
Andrea Cavagna <andrea.cavagna@besharp.it>
2021-04-28 17:50:47 +0200

    refactor: removed accountId and removed type in constructor

--- src/app/models/account.ts
+++ src/app/models/account.ts
@@ -1,16 +1,12 @@
 import {AccountType} from './AccountType';
-import * as uuid from 'uuid';
 
 export class Account {
   accountName: string;
   type: AccountType;
   region: string;
-  accountId: string;
 
-  constructor(accountName: string, type: AccountType, region: string) {
+  constructor(accountName: string, region: string) {
     this.accountName = accountName;
-    this.type = type;
     this.region = region;
-    this.accountId = uuid.v4();
   }
 }

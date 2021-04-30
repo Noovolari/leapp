4c11b9a992048d6da7478cd2c4c019d06f6c1470
Alessandro Gaggia <alex@besharp.it>
2021-04-30 11:41:04 +0200

    test: timeService

--- src/app/services/timer.service.ts
+++ src/app/services/timer.service.ts
@@ -5,14 +5,20 @@ import {Injectable} from '@angular/core';
   providedIn: 'root'
 })
 export class TimerService {
+  set timer(value: NodeJS.Timeout) {
+    this._timer = value;
+  }
+  get timer(): NodeJS.Timeout {
+    return this._timer;
+  }
 
-  timer: NodeJS.Timeout;
-  TIME_INTERVAL = 1000;
+  private _timer: NodeJS.Timeout;
+  private TIME_INTERVAL = 1000;
 
   constructor() { }
 
   start(callback: () => void) {
-    if (this.timer) {
+    if (!this.timer) {
       this.timer = setInterval(() => {
         callback();
       }, this.TIME_INTERVAL);

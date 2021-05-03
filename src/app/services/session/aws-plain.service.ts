7cdd3ca229394ceef93e1d0fdfcffc1aa5c58991
Andrea Cavagna <andrea.cavagna@besharp.it>
2021-05-03 16:38:37 +0200

    refactor: create method for session.service.ts is now abstract, aws-plain-account.ts model access key and secret key are removed, added aws-plain.service.ts create request

--- src/app/services/session/aws-plain.service.ts
+++ src/app/services/session/aws-plain.service.ts
@@ -3,18 +3,33 @@ import {CredentialsInfo} from '../../models/credentials-info';
 import {SessionService} from '../session.service';
 import {WorkspaceService} from '../workspace.service';
 import {AwsPlainAccount} from '../../models/aws-plain-account';
+import {KeychainService} from '../keychain.service';
+import {environment} from '../../../environments/environment';
+import {Session} from '../../models/session';
+import {AccountType} from '../../models/AccountType';
+
+interface AwsPlainAccountRequest {
+  accountName: string;
+  accessKey: string;
+  secretKey: string;
+  region: string;
+  mfaDevice?: string;
+  type: AccountType;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AwsPlainService extends SessionService {
 
-  constructor(private workSpaceService: WorkspaceService) {
+  constructor(private workSpaceService: WorkspaceService, private keychainService: KeychainService) {
     super(workSpaceService);
   }
 
-  create(account: AwsPlainAccount, profileId: string) {
-    super.create(account, profileId);
+  create(accountRequest: AwsPlainAccountRequest, profileId: string) {
+    const session = new Session(new AwsPlainAccount(accountRequest.accountName, accountRequest.region, accountRequest.mfaDevice), profileId);
+    this.saveAccountInfoInKeychain(session, accountRequest);
+    this.addSession(session);
   }
 
   applyCredentials(credentialsInfo: CredentialsInfo): Promise<void> {
@@ -27,5 +42,18 @@ export class AwsPlainService extends SessionService {
 
   generateCredentials(sessionId: string): Promise<CredentialsInfo> {
     return Promise.resolve(undefined);
+  }
+
+  private async saveAccountInfoInKeychain(session: Session, accountRequest: AwsPlainAccountRequest) {
+    await this.keychainService.saveSecret(environment.appName, `${session.sessionId}_access_key`, accountRequest.accessKey);
+    await this.keychainService.saveSecret(environment.appName, `${session.sessionId}_secret_key`, accountRequest.secretKey);
+  }
+
+  private async getAccessKeyFromKeychain(sessionId: string): Promise<string> {
+    return await this.keychainService.getSecret(environment.appName, `${sessionId}_access_key`);
+  }
+
+  private async getSecretKeyFromKeychain(sessionId: string): Promise<string> {
+    return await this.keychainService.getSecret(environment.appName, `${sessionId}_secret_key`);
   }
 }

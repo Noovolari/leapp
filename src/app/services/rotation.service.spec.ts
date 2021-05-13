2a51493f3a4e0d8edc2b8d7f30aadeae5af08750
Alessandro Gaggia <alex@besharp.it>
2021-05-13 12:22:27 +0200

    refactor: rotate service test

--- src/app/services/rotation.service.spec.ts
+++ src/app/services/rotation.service.spec.ts
@@ -1,16 +1,56 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RotationService } from './rotation.service';
+import {mustInjected} from "../../base-injectables";
+import {SessionService} from "./session.service";
+import {SessionProviderService} from "./session-provider.service";
+import {Session} from "../models/session";
+import {Account} from "../models/account";
 
 describe('RotationService', () => {
-  let service: RotationService;
+  let rotationService: RotationService;
+
+  let spySessionService;
+  let sessionActive;
+  let sessionInactive;
+
+  let spySessionProviderService;
+  let concreteSessionServiceFake;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(RotationService);
+    spySessionService = jasmine.createSpyObj('SessionService', ['listActive']);
+    sessionActive = new Session(new Account('fakeaccount', 'eu-west-1'), 'fakeid');
+    sessionActive.active = true;
+    spySessionService.listActive.and.returnValue([sessionActive]);
+
+
+    spySessionProviderService = jasmine.createSpyObj('SessionProviderService', ['getService']);
+    concreteSessionServiceFake = { rotate: () => {} };
+    spySessionProviderService.getService.and.returnValue(concreteSessionServiceFake);
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: SessionService, useValue: spySessionService },
+        { provide: SessionProviderService, useValue: spySessionProviderService },
+      ].concat(mustInjected())
+    });
+    rotationService = TestBed.inject(RotationService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(rotationService).toBeTruthy();
+  });
+
+  describe('rotate()', () => {
+    it('should rotate an active session when expired', () => {
+
+      spyOn(sessionActive, 'expired').and.returnValue(true);
+      spyOn(concreteSessionServiceFake, 'rotate').and.callThrough();
+
+      rotationService.rotate();
+
+      expect(sessionActive.expired).toHaveBeenCalled();
+      expect(concreteSessionServiceFake.rotate).toHaveBeenCalled();
+    });
   });
 });

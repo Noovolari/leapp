bc5218998d11512aa79c8a20f5637a4cd7bf903a
Alessandro Gaggia <alex@besharp.it>
2021-05-17 11:56:08 +0200

    chore: removed unused code from main.ts in Electron chore: removed unused core directory along with AppUpdater, as new code will be introduced chore: moved constants to model

--- src/app/models/account.spec.ts
+++
@@ -1,7 +0,0 @@
-import {Account} from './account';
-
-describe('Account', () => {
-  it('should create an instance', () => {
-    expect(new Account('ciao', 'eu-west-1')).toBeInstanceOf(Account);
-  });
-});
--- src/app/models/aws-plain-account.spec.ts
+++
@@ -1,8 +0,0 @@
-import {AwsPlainAccount} from './aws-plain-account';
-import {AccountType} from './AccountType';
-
-describe('AwsPlainAccount', () => {
-  it('should create an instance', () => {
-    expect(new AwsPlainAccount('', AccountType.AWS_PLAIN_USER, '')).toBeDefined();
-  });
-});

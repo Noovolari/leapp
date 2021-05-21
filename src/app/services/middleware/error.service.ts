bae5c4f59ee56ccf75ceb197dd252d14460701e4
Andrea Cavagna <andrea.cavagna@besharp.it>
2021-05-21 12:04:14 +0200

    chore: removed console.log

--- src/app/services/middleware/error.service.ts
+++ src/app/services/middleware/error.service.ts
@@ -1,6 +1,5 @@
 import {ErrorHandler, Injectable, Injector} from '@angular/core';
 import {AppService} from '../app.service';
-import {LeappBaseError} from '../../errors/leapp-base-error';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +10,6 @@ export class ErrorService implements ErrorHandler {
 
   handleError(error: any): void {
     error = error.rejection ? error.rejection : error;
-    if(error instanceof LeappBaseError) {
-      console.log("Leapp error");
-      console.warn(error);
-    }
     const appService = this.injector.get(AppService);
     appService.logger(error.message, error.severity, error.context, error.stack);
     appService.toast(error.message, error.severity, error.name);

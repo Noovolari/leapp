9109fa5bafc6b3beea3e5c1b77e7ade295503dfb
Alessandro Gaggia <alex@besharp.it>
2021-05-24 12:42:10 +0200

    refactor: aws federated service, model, and aws federated account

--- src/app/models/aws-federated-account.ts
+++ src/app/models/aws-federated-account.ts
@@ -2,15 +2,23 @@ import {SessionType} from './session-type';
 import {Account} from './account';
 
 export class AwsFederatedAccount extends Account {
-  accountId: string;
   accountName: string;
-  accountNumber: string;
-  role: { name: string; roleArn: string; parent?: string; parentRole?: string };
-  idpArn?: string;
-  idpUrl?: string;
+  roleArn: string ;
+  idpArn: string;
+  idpUrl: string;
   type: SessionType;
-  parent?: string;
-  parentRole?: string;
   region: string;
   profileId: string;
+
+  constructor(accountName: string, region: string, idpUrl: string, idpArn: string, roleArn: string, profileId: string) {
+    super(accountName, region);
+    this.type = SessionType.awsFederated;
+
+    this.accountName = accountName;
+    this.region = region;
+    this.idpUrl = idpUrl;
+    this.idpArn = idpArn;
+    this.roleArn = roleArn;
+    this.profileId = profileId;
+  }
 }

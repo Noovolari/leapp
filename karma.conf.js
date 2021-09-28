1420b02ab2aab301bcba575d7cb77d1e4faf1d88
Eric Villa <eric.villa91@gmail.com>
2021-09-28 15:18:19 +0200

    chore: tsconfig.json changed module to esnext

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     files: [
       {

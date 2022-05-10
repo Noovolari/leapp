812a3558b967e1145546d360635b66743c11f2cd
Marco Vanetti <marco.vanetti@gmail.com>
2022-05-10 08:37:17 +0200

    fix: fix node storage test

--- packages/core/src/services/session/azure/azure-service-token-caching-integration.spec.ts
+++ packages/core/src/services/session/azure/azure-service-token-caching-integration.spec.ts
@@ -1,12 +1,12 @@
 import { describe, test } from "@jest/globals";
 import { NodeStorage } from "@azure/msal-node";
-import { FilePersistenceWithDataProtection } from "@azure/msal-node-extensions";
+import { FilePersistence } from "@azure/msal-node-extensions";
 import { homedir } from "os";
 import * as path from "path";
 
 describe("AzureService Integration", () => {
   test("cacheToken", async () => {
-    const cacheFilePersistence = await FilePersistenceWithDataProtection.create(path.join(homedir(), ".azure/msal_token_cache.json"));
+    const cacheFilePersistence = await FilePersistence.create(path.join(homedir(), ".azure/msal_token_cache.json"));
     const cacheFile = await cacheFilePersistence.load();
     const memoryCache = NodeStorage.generateInMemoryCache(cacheFile);
     console.log(memoryCache);

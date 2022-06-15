5e4e7b8b6b7046f191745bcd4417cb38ee5a4765
Marco Vanetti <marco.vanetti@gmail.com>
2022-06-15 14:56:24 +0200

    test: syncSession

--- packages/core/src/services/integration/azure-integration-service-syncSessions.spec.ts
+++ packages/core/src/services/integration/azure-integration-service-syncSessions.spec.ts
@@ -43,9 +43,76 @@ describe("AzureIntegrationService", () => {
     });
   });
 
-  // A local azure session already exists with the same fields of the creation request
-  test("syncSessions, azure local session to keep", async () => {});
+  test("syncSessions, azure local session to keep", async () => {
+    const executeService = { execute: jest.fn() } as any;
+    const azureProfile = {
+      subscriptions: [{ id: "subscriptionId", name: "subscriptionName" }],
+    };
+    const azurePersistenceService = { loadProfile: () => azureProfile } as any;
+    const integrationId = "integrationId";
+    const sessions = [
+      {
+        type: SessionType.azure,
+        sessionName: "subscriptionName",
+        tenantId: "tenantId",
+        subscriptionId: "subscriptionId",
+        region: "region",
+        azureIntegrationId: integrationId,
+      },
+    ];
+    const repository = { getSessions: () => sessions } as any;
+    const azureService = { create: jest.fn() } as any;
+    const service = new AzureIntegrationService(repository, null, null, null, null, executeService, azureService, azurePersistenceService);
 
-  // A local azure session already exists but with at least a different field from the creation request
-  test("syncSessions, azure local session to delete", async () => {});
+    const integration = { tenantId: "tenantId", region: "region" } as any;
+    service.getIntegration = jest.fn(() => integration);
+
+    await service.syncSessions(integrationId);
+
+    expect(service.getIntegration).toHaveBeenCalledWith(integrationId);
+    expect(executeService.execute).toHaveBeenCalledWith("az login --tenant tenantId 2>&1");
+    expect(azureService.create).not.toHaveBeenCalled();
+  });
+
+  test("syncSessions, azure local session to delete", async () => {
+    const executeService = { execute: jest.fn() } as any;
+    const azureProfile = {
+      subscriptions: [{ id: "subscriptionId", name: "subscriptionName" }],
+    };
+    const azurePersistenceService = { loadProfile: () => azureProfile } as any;
+    const integrationId = "integrationId";
+    const sessions = [
+      {
+        sessionId: "sessionId",
+        type: SessionType.azure,
+        sessionName: "differentName",
+        tenantId: "tenantId",
+        subscriptionId: "subscriptionId",
+        region: "region",
+        azureIntegrationId: integrationId,
+      },
+    ];
+    const repository = { getSessions: () => sessions } as any;
+    const azureService = {
+      delete: jest.fn(),
+      create: jest.fn(),
+    } as any;
+    const service = new AzureIntegrationService(repository, null, null, null, null, executeService, azureService, azurePersistenceService);
+
+    const integration = { tenantId: "tenantId", region: "region" } as any;
+    service.getIntegration = jest.fn(() => integration);
+
+    await service.syncSessions(integrationId);
+
+    expect(service.getIntegration).toHaveBeenCalledWith(integrationId);
+    expect(executeService.execute).toHaveBeenCalledWith("az login --tenant tenantId 2>&1");
+    expect(azureService.delete).toHaveBeenCalledWith("sessionId");
+    expect(azureService.create).toHaveBeenCalledWith({
+      region: "region",
+      subscriptionId: "subscriptionId",
+      tenantId: "tenantId",
+      sessionName: "subscriptionName",
+      azureIntegrationId: integrationId,
+    });
+  });
 });

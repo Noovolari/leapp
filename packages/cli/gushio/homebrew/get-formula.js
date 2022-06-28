f237656add5b39d75cd5828170386323aa448bad
Marco Vanetti <marco.vanetti@gmail.com>
2022-06-28 10:46:35 +0200

    chore: added two homebrew formulas to allow CLI installation on darwin-ARM64 platforms

--- packages/cli/gushio/homebrew/get-formula.js
+++ packages/cli/gushio/homebrew/get-formula.js
@@ -1,15 +1,17 @@
-module.exports = (version, tarballUrl, tarballSha256) =>
-  `class LeappCli < Formula
+module.exports = (version, tarballUrl, tarballSha256) => `require "language/node"
+
+class LeappCli < Formula
   desc "Install Leapp CLI"
   homepage "https://leapp.cloud"
   version "${version}"
   url "${tarballUrl}"
   sha256 "${tarballSha256}"
 
+  depends_on "node@16"
+
   def install
-    inreplace "bin/leapp", /^CLIENT_HOME=/, "export LEAPP_OCLIF_CLIENT_HOME=#{lib/"client"}\\nCLIENT_HOME="
-    libexec.install Dir["*"]
-    bin.install_symlink libexec/"bin/leapp"
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec).reject { |a| a == "--build-from-source" }
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do

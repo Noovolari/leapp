b886804c118206f5eefd74c55e95a511f134e327
Alessandro Gaggia <alex@besharp.it>
2022-07-13 18:19:28 +0200

    test: finished test for file-service

--- packages/core/src/services/file-service.spec.ts
+++ packages/core/src/services/file-service.spec.ts
@@ -10,8 +10,6 @@ describe("File Service", () => {
   const fakePath = "fake-path";
 
   let newName;
-  let callbackMock = { err: undefined, data: "data" };
-  let callbackMock2 = { err: undefined, data: null };
 
   let directories: { isDirectory: () => boolean; name: string }[] = [];
 
@@ -31,11 +29,9 @@ describe("File Service", () => {
       existsSync: jest.fn((_path) => _path === existsPath),
       writeFileSync: jest.fn((_file, _data) => {}),
       renameSync: jest.fn((_name, _newName) => (newName = _newName)),
-      readFile: jest.fn((_filePath, _options, _callback: (err: any, data: string) => void) => _callback(callbackMock.err, callbackMock.data)),
       readFileSync: jest.fn((_filePath, _opt: { encoding: "utf-8" }) => "[config]\nfake-key=fake-value\n"),
       readdirSync: jest.fn((_source, _opt: { withFileTypes: true }) => directories),
       mkdirSync: jest.fn((_path, _options) => {}),
-      writeFile: jest.fn((_filePath, content, _callback: (err: any, data: string) => void) => _callback(callbackMock.err, callbackMock.data)),
     },
     copydir: {
       sync: jest.fn((_source, _target, _opt: { mode: true }) => {}),
@@ -77,28 +73,6 @@ describe("File Service", () => {
     expect(fileService.dirname(testPath)).toBe("test/my_test/again_a_test");
     expect(nativeService.path.dirname).toHaveBeenCalledTimes(1);
     expect(nativeService.path.dirname).toHaveBeenCalledWith(testPath);
-  });
-
-  test("readFile, normal and error", async () => {
-    callbackMock = { err: undefined, data: "data" };
-
-    const testPath = "test/my_test/again_a_test/file.js";
-    const fileService = new FileService(nativeService);
-    fileService.readFile(testPath).subscribe(
-      (res) => {
-        expect(res).toBe(callbackMock.data);
-        expect(nativeService.fs.readFile).toHaveBeenNthCalledWith(1, testPath, { encoding: "utf-8" }, callbackMock);
-      },
-      (_err) => {}
-    );
-    callbackMock = { err: "error", data: undefined };
-    fileService.readFile(testPath).subscribe(
-      (_res) => {},
-      (err) => {
-        expect(err).toBe(callbackMock.err);
-        expect(nativeService.fs.readFile).toHaveBeenNthCalledWith(2, testPath, { encoding: "utf-8" }, callbackMock);
-      }
-    );
   });
 
   test("copyDir", () => {
@@ -151,73 +125,12 @@ describe("File Service", () => {
     expect(nativeService.fs.mkdirSync).toHaveBeenCalledTimes(1);
   });
 
-  test("writeFile", () => {
-    callbackMock = { err: undefined, data: "data" };
-    const testPath = "test/my_test/again_a_test/file.js";
-    const fileService = new FileService(nativeService);
-    fileService.writeFile(testPath, "fake-content").subscribe(
-      (res) => {
-        expect(res).toBe(callbackMock.data);
-        expect(nativeService.fs.writeFile).toHaveBeenNthCalledWith(1, testPath, "fake-content", callbackMock);
-      },
-      (_err) => {}
-    );
-    callbackMock = { err: "error", data: undefined };
-    fileService.writeFile(testPath, "fake-content").subscribe(
-      (_res) => {},
-      (err) => {
-        expect(err).toBe(callbackMock.err);
-        expect(nativeService.fs.writeFile).toHaveBeenNthCalledWith(2, testPath, "fake-content", callbackMock);
-      }
-    );
-  });
-
   test("writeFileSync", () => {
     const data = "fake-content";
     const newPath = "new-path";
     const fileService = new FileService(nativeService);
     fileService.writeFileSync(newPath, data);
     expect(nativeService.fs.writeFileSync).toHaveBeenNthCalledWith(1, newPath, data);
-  });
-
-  test("iniWrite", () => {
-    const content = {
-      key1: {
-        subkey1: "",
-        subkey2: "null",
-        subkey3: "fake-value-1",
-      },
-      key2: {
-        subkey4: undefined,
-        subkey5: "fake-value-2",
-        subkey6: null,
-        subkey7: "fake-value-3",
-      },
-      key3: {
-        subkey8: "fake-value-4",
-        subkey9: "fake-value-5",
-      },
-    };
-    const filePath = "new-path";
-    const fileService = new FileService(nativeService);
-    fileService.writeFile = jest.fn();
-    fileService.iniWrite(filePath, content);
-    expect(content).toStrictEqual({
-      key1: {
-        subkey3: "fake-value-1",
-      },
-      key2: {
-        subkey5: "fake-value-2",
-        subkey7: "fake-value-3",
-      },
-      key3: {
-        subkey8: "fake-value-4",
-        subkey9: "fake-value-5",
-      },
-    });
-    expect(fileService.writeFile).toHaveBeenCalledTimes(1);
-    expect(nativeService.ini.stringify).toHaveBeenCalledTimes(1);
-    expect(nativeService.ini.stringify).toHaveBeenCalledWith(content);
   });
 
   test("iniWriteSync", () => {
@@ -317,36 +230,13 @@ describe("File Service", () => {
     expect(fileService.writeFileSync).toHaveBeenCalledWith(filePath, nativeService.ini.stringify(content));
   });
 
-  test("iniParse", async () => {
-    callbackMock2 = { err: undefined, data: { data: "fake-data" } };
-    const filePath = "new-path";
-    const fileService = new FileService(nativeService);
-    jest.spyOn(fileService, "readFile");
-    (fileService as any).readSubscription = { unsubscribe: jest.fn() } as any;
-    fileService.iniParse(filePath).subscribe(
-      (res) => {
-        expect(res).toBe(nativeService.ini.parse(callbackMock2.data));
-        expect(fileService.readFile).toHaveBeenCalled();
-        expect((fileService as any).readSubscription.unsubscribe).toHaveBeenCalled();
-      },
-      (_err) => {}
-    );
-    callbackMock2 = { err: "Error", data: undefined };
-    fileService.iniParse(filePath).subscribe(
-      (_res) => {},
-      (err) => {
-        expect(err).toBe(callbackMock2.err);
-      }
-    );
-  });
-
   test("iniParseSync", () => {
     const filePath = "file-path";
     const fileService = new FileService(nativeService);
     fileService.readFileSync = jest.fn(() => "fake-content");
     fileService.iniParseSync(filePath);
     expect(fileService.readFileSync).toHaveBeenCalled();
-    expect(nativeService.ini.parse).toHaveBeenNthCalledWith(1, "fake-content");
+    expect(nativeService.ini.parse).toHaveBeenCalledWith("fake-content");
   });
 
   test("encryptText", () => {

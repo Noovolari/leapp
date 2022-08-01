d0b58b6f3a05f9551374634840341f82fb0ff015
Riccardo Rotta <riccardo.rotta@besharp.it>
2022-08-01 16:41:49 +0200

    chore: added sample plugin open web console

---
+++ plugins-sdk/web-console-plugin.ts
@@ -0,0 +1,65 @@
+import { Session } from "@noovolari/leapp-core/models/session";
+import { LoggedEntry, LogLevel } from "@noovolari/leapp-core/services/log-service";
+import { AwsSessionService } from "../packages/core/dist/services/session/aws/aws-session-service";
+
+export class WebConsolePlugin {
+  public metadata: any;
+
+  private providerService: any;
+
+  async bootstrap(pluginEnvironment: any): Promise<void> {
+    this.providerService = pluginEnvironment.providerService;
+  }
+
+  async applySessionAction(session: Session): Promise<void> {
+    this.providerService.logService.log(new LoggedEntry("Opening web console for session: " + session.sessionName, this, LogLevel.info, true));
+
+    console.log(session.sessionId);
+
+    const sessionRegion = session.region;
+
+    //const credentialsInfo = await (this.sessionService as AwsSessionService).generateCredentials(this.session.sessionId);
+    const credentialsInfo = await (this.providerService.sessionFactory.getSessionService(session.type) as AwsSessionService).generateCredentials(
+      session.sessionId
+    );
+    const sessionDuration = 3200;
+    const fetch = window.fetch.bind(window);
+
+    const isUSGovCloud = sessionRegion.startsWith("us-gov-");
+
+    let federationUrl;
+    let consoleHomeURL;
+
+    if (!isUSGovCloud) {
+      federationUrl = "https://signin.aws.amazon.com/federation";
+      consoleHomeURL = `https://${sessionRegion}.console.aws.amazon.com/console/home?region=${sessionRegion}`;
+    } else {
+      federationUrl = "https://signin.amazonaws-us-gov.com/federation";
+      consoleHomeURL = `https://console.amazonaws-us-gov.com/console/home?region=${sessionRegion}`;
+    }
+
+    if (sessionRegion.startsWith("cn-")) {
+      throw new Error("Unsupported Region");
+    }
+
+    this.providerService.logService.log(new LoggedEntry("Starting opening Web Console", this, LogLevel.info));
+
+    const sessionStringJSON = {
+      sessionId: credentialsInfo.sessionToken.aws_access_key_id,
+      sessionKey: credentialsInfo.sessionToken.aws_secret_access_key,
+      sessionToken: credentialsInfo.sessionToken.aws_session_token,
+    };
+
+    const queryParametersSigninToken = `?Action=getSigninToken&SessionDuration=${sessionDuration}&Session=${encodeURIComponent(
+      JSON.stringify(sessionStringJSON)
+    )}`;
+
+    const res = await fetch(`${federationUrl}${queryParametersSigninToken}`);
+    const response = await res.json();
+
+    const loginURL = `${federationUrl}?Action=login&Issuer=Leapp&Destination=${consoleHomeURL}&SigninToken=${(response as any).SigninToken}`;
+
+    //this.providerService.shellService.openExternalUrl(loginUrl);
+    this.providerService.windowService.openExternalUrl(loginURL);
+  }
+}
--- plugins-sdk/webpack.config.js
+++ plugins-sdk/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './testPlugin.ts',
+  entry: './web-console-plugin.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'plugin.js',

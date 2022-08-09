258aed4986ee95e4f71e40c8073a509ce9bb4e95
Alessandro Gaggia <alex@besharp.it>
2022-08-09 14:54:32 +0200

    test: added all tests for integrtion factory with coverage 100%

--- packages/core/src/services/integration-factory.spec.ts
+++ packages/core/src/services/integration-factory.spec.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, test, expect } from "@jest/globals";
+import { beforeEach, describe, test, expect, jest } from "@jest/globals";
 import { IntegrationFactory } from "./integration-factory";
 import { IntegrationType } from "../models/integration-type";
 import { AwsSsoIntegrationService } from "./integration/aws-sso-integration-service";
@@ -34,19 +34,139 @@ describe("IntegrationFactory", () => {
     expect(fakeIntegrationService.createIntegration).toHaveBeenCalledWith("fake-integration-params");
   });
 
-  test("update", async () => {});
+  test("update", async () => {
+    const fakeAzureIntegration = { id: "fake-id-azure", tenantId: "fake-tenant-1" };
+    azureIntegrationService.getIntegration = jest.fn(() => fakeAzureIntegration);
+    azureIntegrationService.logout = jest.fn();
 
-  test("delete", async () => {});
+    const fakeIntegrationService = {
+      updateIntegration: jest.fn(async () => {}),
+    } as any;
 
-  test("syncSessions", async () => {});
+    (factory as any).azureIntegrationService = azureIntegrationService;
+    factory.getIntegrationById = jest.fn(() => IntegrationType.awsSso);
+    factory.getIntegrationService = jest.fn(() => fakeIntegrationService);
+    await factory.update("fake-id", "fake-params");
+    expect(factory.getIntegrationById).toHaveBeenCalledWith("fake-id");
+    expect(factory.getIntegrationById).toHaveReturnedWith(IntegrationType.awsSso);
+    expect(factory.getIntegrationService).toHaveReturnedWith(fakeIntegrationService);
+    expect(fakeIntegrationService.updateIntegration).toHaveBeenCalledWith("fake-id", "fake-params");
 
-  test("logout", async () => {});
+    factory.getIntegrationById = jest.fn(() => ({ type: IntegrationType.azure }));
+    await factory.update("fake-id-2", { id: "fake-params-2", tenantId: "fake-tenant-1" });
+    expect(factory.getIntegrationById).toHaveBeenCalledWith("fake-id-2");
+    expect(factory.getIntegrationById).toHaveReturnedWith({ type: IntegrationType.azure });
+    expect(factory.getIntegrationService).toHaveReturnedWith(fakeIntegrationService);
+    expect(fakeIntegrationService.updateIntegration).toHaveBeenNthCalledWith(2, "fake-id-2", { id: "fake-params-2", tenantId: "fake-tenant-1" });
+    expect(azureIntegrationService.getIntegration).toHaveBeenCalledWith("fake-id-2");
+    expect(azureIntegrationService.getIntegration).toHaveReturnedWith(fakeAzureIntegration);
 
-  test("setOnline", async () => {});
+    await factory.update("fake-id-3", { id: "fake-params-3", tenantId: "fake-tenant-2" });
+    expect(factory.getIntegrationById).toHaveBeenCalledWith("fake-id-3");
+    expect(factory.getIntegrationById).toHaveReturnedWith({ type: IntegrationType.azure });
+    expect(factory.getIntegrationService).toHaveReturnedWith(fakeIntegrationService);
+    expect(fakeIntegrationService.updateIntegration).toHaveBeenNthCalledWith(3, "fake-id-3", { id: "fake-params-3", tenantId: "fake-tenant-2" });
+    expect(azureIntegrationService.getIntegration).toHaveBeenCalledWith("fake-id-3");
+    expect(azureIntegrationService.getIntegration).toHaveReturnedWith(fakeAzureIntegration);
+    expect(azureIntegrationService.logout).toHaveBeenCalledWith("fake-id-3");
+  });
 
-  test("getRemainingHours", () => {});
+  test("delete", async () => {
+    const fakeIntegrationService = {
+      deleteIntegration: jest.fn(async () => {}),
+    } as any;
 
-  test("getIntegrations", () => {});
+    (factory as any).azureIntegrationService = azureIntegrationService;
+    factory.getIntegrationById = jest.fn(() => ({ type: IntegrationType.awsSso }));
+    factory.getIntegrationService = jest.fn(() => fakeIntegrationService);
 
-  test("getIntegrationById", () => {});
+    await factory.delete("fake-id");
+    expect(factory.getIntegrationById).toHaveBeenCalledWith("fake-id");
+    expect(factory.getIntegrationById).toHaveReturnedWith({ type: IntegrationType.awsSso });
+    expect(factory.getIntegrationService).toHaveBeenCalledWith(IntegrationType.awsSso);
+    expect(fakeIntegrationService.deleteIntegration).toHaveBeenCalledWith("fake-id");
+  });
+
+  test("syncSessions", async () => {
+    const fakeIntegrationService = {
+      syncSessions: jest.fn(async () => {}),
+    } as any;
+
+    (factory as any).azureIntegrationService = azureIntegrationService;
+    factory.getIntegrationById = jest.fn(() => ({ type: IntegrationType.awsSso }));
+    factory.getIntegrationService = jest.fn(() => fakeIntegrationService);
+
+    await factory.syncSessions("fake-id");
+    expect(factory.getIntegrationById).toHaveBeenCalledWith("fake-id");
+    expect(factory.getIntegrationById).toHaveReturnedWith({ type: IntegrationType.awsSso });
+    expect(factory.getIntegrationService).toHaveBeenCalledWith(IntegrationType.awsSso);
+    expect(fakeIntegrationService.syncSessions).toHaveBeenCalledWith("fake-id");
+  });
+
+  test("logout", async () => {
+    const fakeIntegrationService = {
+      logout: jest.fn(async () => {}),
+    } as any;
+
+    (factory as any).azureIntegrationService = azureIntegrationService;
+    factory.getIntegrationById = jest.fn(() => ({ type: IntegrationType.awsSso }));
+    factory.getIntegrationService = jest.fn(() => fakeIntegrationService);
+
+    await factory.logout("fake-id");
+    expect(factory.getIntegrationById).toHaveBeenCalledWith("fake-id");
+    expect(factory.getIntegrationById).toHaveReturnedWith({ type: IntegrationType.awsSso });
+    expect(factory.getIntegrationService).toHaveBeenCalledWith(IntegrationType.awsSso);
+    expect(fakeIntegrationService.logout).toHaveBeenCalledWith("fake-id");
+  });
+
+  test("setOnline", async () => {
+    const fakeIntegrationService = {
+      setOnline: jest.fn(async () => {}),
+    } as any;
+
+    (factory as any).azureIntegrationService = azureIntegrationService;
+    factory.getIntegrationById = jest.fn(() => ({ type: IntegrationType.awsSso }));
+    factory.getIntegrationService = jest.fn(() => fakeIntegrationService);
+
+    await factory.setOnline({ id: "fake-id" });
+    expect(factory.getIntegrationById).toHaveBeenCalledWith("fake-id");
+    expect(factory.getIntegrationById).toHaveReturnedWith({ type: IntegrationType.awsSso });
+    expect(factory.getIntegrationService).toHaveBeenCalledWith(IntegrationType.awsSso);
+    expect(fakeIntegrationService.setOnline).toHaveBeenCalledWith({ id: "fake-id" });
+  });
+
+  test("getRemainingHours", () => {
+    const fakeIntegrationService = {
+      remainingHours: jest.fn(async () => {}),
+    } as any;
+
+    (factory as any).azureIntegrationService = azureIntegrationService;
+    factory.getIntegrationById = jest.fn(() => ({ type: IntegrationType.awsSso }));
+    factory.getIntegrationService = jest.fn(() => fakeIntegrationService);
+
+    factory.getRemainingHours({ id: "fake-id" });
+    expect(factory.getIntegrationById).toHaveBeenCalledWith("fake-id");
+    expect(factory.getIntegrationById).toHaveReturnedWith({ type: IntegrationType.awsSso });
+    expect(factory.getIntegrationService).toHaveBeenCalledWith(IntegrationType.awsSso);
+    expect(fakeIntegrationService.remainingHours).toHaveBeenCalledWith({ id: "fake-id" });
+  });
+
+  test("getIntegrations", () => {
+    awsSsoIntegrationService.getIntegrations = jest.fn(() => ["1", "2"]);
+    azureIntegrationService.getIntegrations = jest.fn(() => ["3", "4"]);
+    (factory as any).awsSsoIntegrationService = awsSsoIntegrationService;
+    (factory as any).azureIntegrationService = azureIntegrationService;
+    const result = factory.getIntegrations();
+    expect(result).toStrictEqual([...awsSsoIntegrationService.getIntegrations(), ...azureIntegrationService.getIntegrations()]);
+  });
+
+  test("getIntegrationById", () => {
+    awsSsoIntegrationService.getIntegrations = jest.fn(() => [{ id: "1" }, { id: "2" }]);
+    azureIntegrationService.getIntegrations = jest.fn(() => [{ id: "3" }, { id: "4" }]);
+    (factory as any).awsSsoIntegrationService = awsSsoIntegrationService;
+    (factory as any).azureIntegrationService = azureIntegrationService;
+
+    const result = factory.getIntegrationById("1");
+    expect(result).toStrictEqual({ id: "1" });
+  });
 });

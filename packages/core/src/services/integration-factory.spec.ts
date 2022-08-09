214c595554171d02e9b8e9a0e137d60afbd3d059
Riccardo Rotta <riccardorotta@gmail.com>
2022-08-09 13:01:43 +0200

    test: started testing integration factory service

--- packages/core/src/services/integration-factory.spec.ts
+++ packages/core/src/services/integration-factory.spec.ts
@@ -1,17 +1,38 @@
-import { describe, test, beforeEach } from "@jest/globals";
+import { beforeEach, describe, test, expect } from "@jest/globals";
 import { IntegrationFactory } from "./integration-factory";
+import { IntegrationType } from "../models/integration-type";
+import { AwsSsoIntegrationService } from "./integration/aws-sso-integration-service";
+import { AzureIntegrationService } from "./integration/azure-integration-service";
 
 describe("IntegrationFactory", () => {
   // eslint-disable-next-line no-unused-vars
   let factory;
+  let awsSsoIntegrationService;
+  let azureIntegrationService;
 
   beforeEach(() => {
+    azureIntegrationService = new AzureIntegrationService(null, null, null, null, null, null, null);
+    awsSsoIntegrationService = new AwsSsoIntegrationService(null, null, null, null, null, null, null);
     factory = new IntegrationFactory(null, null);
   });
 
-  test("getIntegrationService", () => {});
+  test("getIntegrationService", () => {
+    factory = new IntegrationFactory(awsSsoIntegrationService, azureIntegrationService);
+    let returnValue = factory.getIntegrationService(IntegrationType.awsSso);
+    expect(returnValue).toBe(awsSsoIntegrationService);
+    returnValue = factory.getIntegrationService(IntegrationType.azure);
+    expect(returnValue).toBe(azureIntegrationService);
+  });
 
-  test("create", async () => {});
+  test("create", async () => {
+    const fakeIntegrationService = {
+      createIntegration: jest.fn(async () => {}),
+    } as any;
+    factory.getIntegrationService = jest.fn(() => fakeIntegrationService);
+    await factory.create("fake-integration-type" as any, "fake-integration-params" as any);
+    expect(factory.getIntegrationService).toHaveBeenCalledWith("fake-integration-type");
+    expect(fakeIntegrationService.createIntegration).toHaveBeenCalledWith("fake-integration-params");
+  });
 
   test("update", async () => {});
 

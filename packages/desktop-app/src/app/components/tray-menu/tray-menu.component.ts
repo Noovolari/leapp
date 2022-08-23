73579936520343b85fde468d1c5a96906d697c06
Riccardo Rotta <riccardorotta@gmail.com>
2022-08-23 17:15:19 +0200

    chore: changed error message when session manager plugin is missing

--- packages/desktop-app/src/app/components/tray-menu/tray-menu.component.ts
+++ packages/desktop-app/src/app/components/tray-menu/tray-menu.component.ts
@@ -268,7 +268,7 @@ export class TrayMenuComponent implements OnInit, OnDestroy {
         throw new LeappLinkError(
           "https://docs.leapp.cloud/latest/built-in-features/aws-ec2-connect/",
           this,
-          "An error occurred getting AWS Session Manager Plugin version. Please <span class='link'>click this message</span> to install it."
+          "An error occurred getting AWS Session Manager Plugin version. <span class='link'>Follow the instructions on the docs</span> to solve the issue."
         );
       }
     }

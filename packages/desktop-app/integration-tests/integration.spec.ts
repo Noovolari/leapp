88116805ce0ef3ee9dd8596b6f3d400ca7d778cc
Marco Vanetti <marco.vanetti@gmail.com>
2022-09-15 18:03:25 +0200

    chore: testing chromewebdriver and selenium to control desktop app

---
+++ packages/desktop-app/integration-tests/integration.spec.ts
@@ -0,0 +1,66 @@
+import { describe, test } from "@jest/globals";
+import { Builder, By, until } from "selenium-webdriver";
+
+describe("Integration test 1", () => {
+  let driver;
+
+  // beforeAll(async () => {
+  //   await new Promise<void>((resolve, reject) => {
+  //     try {
+  //       chromeDriver = spawn("../node_modules/.bin/chromedriver");
+  //
+  //       chromeDriver.on("close", (code, signal) => {
+  //         console.log(`child process terminated due to receipt of signal ${signal}`);
+  //       });
+  //
+  //       chromeDriver.stdout.on("data", (data) => {
+  //         if (data.includes("ChromeDriver was started successfully.")) {
+  //           resolve();
+  //         }
+  //       });
+  //
+  //       chromeDriver.stderr.on("data", (data) => {
+  //         reject(data);
+  //       });
+  //     } catch (error) {
+  //       reject(error);
+  //     }
+  //   });
+  // }, 5000000);
+
+  beforeEach(async () => {
+    driver = await new Builder()
+      .usingServer("http://localhost:9515")
+      .withCapabilities({
+        "goog:chromeOptions": {
+          binary: "/Users/marcovanetti/Repos/leapp/packages/desktop-app/node_modules/electron/dist/Electron.app/Contents/MacOS/Electron",
+          args: ["app=/Users/marcovanetti/Repos/leapp/packages/desktop-app"],
+        },
+      })
+      .forBrowser("chrome")
+      .build();
+  });
+
+  afterEach(async () => {
+    await driver.quit();
+  });
+
+  // afterAll(() => {
+  //   chromeDriver.kill("SIGTERM");
+  // });
+
+  test("my integration test 1", async () => {
+    const addSessionButton = await driver.wait(until.elementLocated(By.css('button[mattooltip="Add a new Session"]')), 10000);
+    await addSessionButton.click();
+
+    const strategyButtonSelector = By.css(".strategy-list button");
+    await driver.wait(until.elementLocated(strategyButtonSelector));
+    const strategyButtons = await driver.findElements(strategyButtonSelector);
+    expect(strategyButtons.length).toBe(3);
+
+    const buttons = await Promise.all(strategyButtons.map((button) => (async () => ({ button, text: await button.getText() }))()));
+    const awsButton = buttons.find((button) => button.text.includes("AWS")).button;
+
+    await awsButton.click();
+  }, 500000);
+});

322e0e10c2d8029d2be0a8cb0f628e9537ca8631
Riccardo Rotta <riccardorotta@gmail.com>
2022-09-19 12:58:49 +0200

    chore: added create session integration test and various helper functions

--- packages/desktop-app/integration-tests/integration.spec.ts
+++ packages/desktop-app/integration-tests/integration.spec.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "@jest/globals";
-import { Builder, By, until } from "selenium-webdriver";
+import { Builder, By, until, WebElement } from "selenium-webdriver";
 import * as path from "path";
 import * as os from "os";
 
@@ -14,6 +14,13 @@ describe("Integration test 1", () => {
     linux: linuxPath,
     win32: winPath,
   };
+
+  const pause = (timeout) =>
+    new Promise((resolve, _) => {
+      setTimeout(() => {
+        resolve(true);
+      }, timeout);
+    });
 
   beforeEach(async () => {
     driver = await new Builder()
@@ -32,9 +39,24 @@ describe("Integration test 1", () => {
     await driver.quit();
   }, testTimeout);
 
+  const selectElementByCss = async (selector: string): Promise<WebElement> => await driver.wait(until.elementLocated(By.css(selector)), 10000);
+
   const clickOnAddSessionButton = async () => {
-    const addSessionButton = await driver.wait(until.elementLocated(By.css('button[mattooltip="Add a new Session"]')), 10000);
+    const addSessionButton = await selectElementByCss('button[mattooltip="Add a new Session"]');
     await addSessionButton.click();
+  };
+
+  const selectElementWithInnerText = async (text: string, selector: By) => {
+    const elements = await driver.findElements(selector);
+    const element = await Promise.all(elements.map((el) => (async () => ({ el, text: await el.getText() }))()));
+    return element.find((el) => el.text.includes(text)).el;
+  };
+
+  const clickOnStrategyButton = async (strategy: string) => {
+    const strategyButtonSelector = By.css(".strategy-list button");
+    await driver.wait(until.elementLocated(strategyButtonSelector));
+    const awsButton = await selectElementWithInnerText(strategy, strategyButtonSelector);
+    await awsButton.click();
   };
 
   test(
@@ -51,6 +73,41 @@ describe("Integration test 1", () => {
       const awsButton = buttons.find((button) => button.text.includes("AWS")).button;
 
       await awsButton.click();
+    },
+    testTimeout
+  );
+
+  test(
+    "create session",
+    async () => {
+      await clickOnAddSessionButton();
+      await clickOnStrategyButton("AWS");
+      const sessionTypeOption = await selectElementByCss('ng-select[placeholder="Select Session Strategy"]');
+      await sessionTypeOption.click();
+
+      const selectedSessionType = await selectElementWithInnerText("AWS IAM User", By.css("div span.ng-option-label"));
+      await selectedSessionType.click();
+
+      const sessionAlias = await selectElementByCss('input[placeholder="Session Alias *"]');
+      await sessionAlias.sendKeys("selenium-session");
+
+      const sessionAccessKey = await selectElementByCss('input[placeholder="Access Key ID *"]');
+      await sessionAccessKey.sendKeys("a");
+
+      const sessionSecretAccessKey = await selectElementByCss('input[placeholder="Secret Access Key *"]');
+      await sessionSecretAccessKey.sendKeys("a");
+
+      const createButton = await selectElementWithInnerText("Create Session", By.css("button"));
+      await createButton.click();
+
+      const seleniumSession = await selectElementWithInnerText("selenium-session", By.css("td div.session-name-td"));
+      await seleniumSession.click();
+
+      const playButton = await selectElementByCss("a.start-session");
+
+      await pause(2000);
+
+      await playButton.click();
     },
     testTimeout
   );

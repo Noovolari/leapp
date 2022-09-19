577037665a09965dfb310cf1ee586391b3d3d1a5
Alessandro Gaggia <alex@besharp.it>
2022-09-19 16:34:38 +0200

    chore: fixed gushio script

--- packages/desktop-app/gushio/create-pipeline-testing-env-file.js
+++ packages/desktop-app/gushio/create-pipeline-testing-env-file.js
@@ -6,25 +6,19 @@ module.exports = {
     arguments: [{ name: "<env_secret>", description: "the github action secret" },],
   },
   run: async (args) => {
-    const envJson = JSON.parse(args.env_secret);
+    const [env_secret] = args;
+    const envJson = JSON.parse(env_secret);
     const path = require("path");
     const shellJs = require('shelljs');
+    const os = require('os');
     const currentOS = os.platform();
     const integrationTestsPath = path.join(__dirname, "..", "integration-tests");
 
     try {
       let result;
       if(currentOS !== "win32") {
-        result = shellJs.exec(`JSON_TEST_ENV='export const env = {
-          "awsIamUserTest": {
-            "accessKeyId": "%s",
-            "secretAccessKey": "%s"
-          }
-        };'`);
-        if (result.code !== 0) {
-          throw new Error(result.stderr)
-        }
-        result = shellJs.exec(`printf "JSON_TEST_ENV" "${envJson.awsIamUserTest.accessKeyId}" "${envJson.awsIamUserTest.secretAccessKey}" > ${integrationTestsPath}/.env.ts`);
+        const jsonText = `JSON_TEST_ENV='export const env = {\n\t"awsIamUserTest": {\n\t\t"accessKeyId": "%s",\n\t\t"secretAccessKey": "%s"\n\t}\n};\n'`
+        result = shellJs.exec(`${jsonText} && printf "$JSON_TEST_ENV" "${envJson.awsIamUserTest.accessKeyId}" "${envJson.awsIamUserTest.secretAccessKey}" > ${integrationTestsPath}/.env.ts`);
         if (result.code !== 0) {
           throw new Error(result.stderr)
         }

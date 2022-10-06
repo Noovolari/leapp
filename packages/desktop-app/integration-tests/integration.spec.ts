4c82df6324b55c66b0029e34284c201035c91c10
Marco Vanetti <marco.vanetti@gmail.com>
2022-10-06 14:37:54 +0200

    chore: integration tests attempt 33

--- packages/desktop-app/integration-tests/integration.spec.ts
+++ packages/desktop-app/integration-tests/integration.spec.ts
@@ -4,6 +4,7 @@ import chromedriver from "chromedriver";
 import { ThenableWebDriver, WebElement, Builder, By, until } from "selenium-webdriver";
 import * as path from "path";
 import * as os from "os";
+// import { STSClient, GetCallerIdentityCommand, GetCallerIdentityCommandInput } from "@aws-sdk/client-sts";
 
 const linuxPath = path.resolve(".", "node_modules/electron/dist/electron");
 const macPath = path.resolve(".", "node_modules/electron/dist/Electron.app/Contents/MacOS/Electron");
@@ -30,10 +31,8 @@ export const generateDriver = async (): Promise<any> => {
     .build();
 };
 
-export const selectElementByCss = async (selector: string, driver: ThenableWebDriver): Promise<WebElement> => {
-  console.log("waiting 60 seconds - driver wait");
-  return driver.wait(until.elementLocated(By.css(selector)), 60000);
-};
+export const selectElementByCss = async (selector: string, driver: ThenableWebDriver): Promise<WebElement> =>
+  driver.wait(until.elementLocated(By.css(selector)), 60000);
 
 export const clickOnAddSessionButton = async (driver: ThenableWebDriver): Promise<void> => {
   const addSessionButton = await selectElementByCss('button[mattooltip="Add a new Session"]', driver);
@@ -63,75 +62,69 @@ export const pause = (timeout: number): Promise<boolean> =>
 
 export const waitUntilDisplayed = (selector: string, expectDisplayToBe: boolean, driver: ThenableWebDriver): Promise<boolean> =>
   new Promise((resolve, _) => {
-    console.log("in wait until displayed...");
+    let intervalId = null;
     const timeoutId = setTimeout(() => {
       // Resolve in case of unexpected errors that would make the method run endlessly
       clearTimeout(timeoutId);
+      clearInterval(intervalId);
       resolve(false);
     }, 20000);
 
-    const intervalId = setInterval(async () => {
+    intervalId = setInterval(async () => {
       if (expectDisplayToBe === false) {
         // Resolve when the element is not present in the page anymore throwing a not such element exception
         try {
-          console.log("executing set interval callback...");
           await driver.findElement(By.css(selector));
         } catch (err) {
           clearInterval(intervalId);
+          clearTimeout(timeoutId);
           resolve(true);
         }
       } else {
         try {
           await driver.findElement(By.css(selector));
           clearInterval(intervalId);
+          clearTimeout(timeoutId);
           resolve(true);
         } catch (err) {}
       }
     }, 500);
   });
 
-describe("Integration test 1", () => {
+describe("Leapp integration tests", () => {
   const testTimeout = 60000;
   let driver;
 
-  beforeAll(async () => {}, testTimeout);
+  beforeAll(async () => {
+    await chromedriver.start(undefined, true);
+  }, testTimeout);
 
-  afterAll(async () => {}, testTimeout);
+  afterAll(async () => {
+    chromedriver.stop();
+    await pause(1000);
+  }, testTimeout);
 
   beforeEach(async () => {
-    console.log("in before each...");
     try {
-      await chromedriver.start(undefined, true);
-      console.log("chromedriver started successfully");
-      await pause(1000);
       driver = await generateDriver();
-      console.log("driver generated successfully");
-      await pause(5000);
     } catch (err) {
       console.error(err);
     }
   }, testTimeout);
 
   afterEach(async () => {
-    console.log("in after each...");
     await driver.quit();
-    console.log("driver quit successfully");
-    chromedriver.stop();
-    console.log("chromedriver stop successfully");
   }, testTimeout);
 
   test(
-    "my integration test 1",
+    "Integration test 1",
     async () => {
-      console.log("in integration test 1...");
       await clickOnAddSessionButton(driver);
 
       const strategyButtonSelector = By.css(".strategy-list button");
       await driver.wait(until.elementLocated(strategyButtonSelector));
       const strategyButtons = await driver.findElements(strategyButtonSelector);
       expect(strategyButtons.length).toBe(4);
-
-      console.log("expect executed correctly...");
 
       const buttons = await Promise.all(strategyButtons.map((button) => (async () => ({ button, text: await button.getText() }))()));
       const awsButton = buttons.find((button) => button.text.includes("AWS")).button;
@@ -142,25 +135,15 @@ describe("Integration test 1", () => {
   );
 
   test(
-    "create session",
+    "Integration test 2",
     async () => {
-      console.log("in integration test 2...");
-
-      console.log("before clickOnAddSessionButton...");
       await clickOnAddSessionButton(driver);
-      console.log("before clickOnStrategyButton...");
       await clickOnStrategyButton("AWS", driver);
-      console.log("before selectElementByCss...");
       const sessionTypeOption = await selectElementByCss('ng-select[placeholder="Select Session Strategy"]', driver);
-      console.log("before click...");
       await sessionTypeOption.click();
 
-      console.log("before selectElementWithInnerText...");
       const selectedSessionType = await selectElementWithInnerText("AWS IAM User", By.css("div span.ng-option-label"), driver);
-      console.log("before click 2...");
       await selectedSessionType.click();
-
-      console.log("about to create a session...");
 
       const sessionAlias = await selectElementByCss('input[placeholder="Session Alias *"]', driver);
       await sessionAlias.sendKeys("selenium-session");
@@ -171,8 +154,6 @@ describe("Integration test 1", () => {
       const sessionSecretAccessKey = await selectElementByCss('input[placeholder="Secret Access Key *"]', driver);
       await sessionSecretAccessKey.sendKeys(env.awsIamUserTest.secretAccessKey);
 
-      console.log("pressing the button...");
-
       const createButton = await selectElementWithInnerText("Create Session", By.css("button"), driver);
       await createButton.click();
 
@@ -180,12 +161,30 @@ describe("Integration test 1", () => {
       await seleniumSession.click();
 
       const playButton = await selectElementByCss("a.start-session", driver);
-
-      console.log("before wait until displayed..");
-
       await waitUntilDisplayed("div.holder", false, driver);
-
       await playButton.click();
+
+      await seleniumSession.click();
+
+      // const stopButton = await selectElementByCss("a.stop-session", driver);
+      //
+      // const client = new STSClient({});
+      // const command = new GetCallerIdentityCommand({});
+      // const validResponse = await client.send(command);
+      // expect(validResponse).toMatchObject({
+      //   ["UserId"]: "xxx",
+      //   ["Account"]: "xxx",
+      //   ["Arn"]: "xxx",
+      // });
+      //
+      // await stopButton.click();
+      //
+      // await seleniumSession.click();
+      // await selectElementByCss("a.start-session", driver);
+      //
+      // const client2 = new STSClient({});
+      // const invalidResponse = await client2.send(command);
+      // console.log(invalidResponse);
     },
     testTimeout
   );

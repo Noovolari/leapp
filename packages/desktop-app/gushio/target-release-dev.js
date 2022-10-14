01971c6994650960feabd30008566ffae34fbcd2
Riccardo Rotta <riccardorotta@gmail.com>
2022-10-14 11:27:24 +0200

    chore: changed nightly github action

--- packages/desktop-app/gushio/target-release-dev.js
+++ packages/desktop-app/gushio/target-release-dev.js
@@ -38,7 +38,7 @@ module.exports = {
 
       let command;
       if (args[1] === 'mac') {
-        command = `export CSC_IDENTITY_AUTO_DISCOVERY=false && electron-builder build ${platformVersion}`;
+        command = `export CSC_IDENTITY_AUTO_DISCOVERY=true && electron-builder build ${platformVersion}`;
       } else {
         command = `electron-builder build ${platformVersion}`;
       }

11194c26bfd133e0291b68f4862572e9957e4788
Alessandro Gaggia <alex@besharp.it>
2022-10-21 16:16:04 +0200

    chore: added gushio script for making tests and generating badges

--- gushio/make-badges.js
+++ gushio/make-badges.js
@@ -2,38 +2,46 @@ module.exports = {
   cli: {
     name: 'make-badges',
     description: 'Make coverage badges for the README.md file',
-    version: '0.1'
+    version: '0.1',
+    arguments: [
+      {name: '<target>', choices: ['core', 'cli', 'desktop-app', 'all']},
+    ],
   },
   run: async (args) => {
     const shellJs = require('shelljs');
     const currentPath = shellJs.pwd();
+    let result;
     try {
-      // Core badge
-      let result = shellJs.exec('cd packages/core && npm run test');
-      if (result.code !== 0) {
-        throw new Error(result.stderr)
-      }
-      result = shellJs.exec('npx coverage-badges --source packages/core/coverage/coverage-summary.json --label "core coverage"');
-      if (result.code !== 0) {
-        throw new Error(result.stderr)
-      }
-      result = shellJs.exec('mv coverage/badges.svg coverage/core-badges.svg');
-      if (result.code !== 0) {
-        throw new Error(result.stderr)
+      if(args[0] === 'core' || args[0] == 'all') {
+        // Core badge
+        result = shellJs.exec('cd packages/core && npm run test');
+        if (result.code !== 0) {
+          throw new Error(result.stderr)
+        }
+        result = shellJs.exec('npx coverage-badges --source packages/core/coverage/coverage-summary.json --label "core coverage"');
+        if (result.code !== 0) {
+          throw new Error(result.stderr)
+        }
+        result = shellJs.exec('mv coverage/badges.svg coverage/core-badges.svg');
+        if (result.code !== 0) {
+          throw new Error(result.stderr)
+        }
       }
 
-      // Cli badge
-      result = shellJs.exec('cd packages/cli && npm run test');
-      if (result.code !== 0) {
-        throw new Error(result.stderr)
-      }
-      result = shellJs.exec('npx coverage-badges --source packages/cli/coverage/coverage-summary.json --label "cli coverage"');
-      if (result.code !== 0) {
-        throw new Error(result.stderr)
-      }
-      result = shellJs.exec('mv coverage/badges.svg coverage/cli-badges.svg');
-      if (result.code !== 0) {
-        throw new Error(result.stderr)
+      if(args[0] === 'cli' || args[0] == 'all') {
+        // Cli badge
+        result = shellJs.exec('cd packages/cli && npm run test');
+        if (result.code !== 0) {
+          throw new Error(result.stderr)
+        }
+        result = shellJs.exec('npx coverage-badges --source packages/cli/coverage/coverage-summary.json --label "cli coverage"');
+        if (result.code !== 0) {
+          throw new Error(result.stderr)
+        }
+        result = shellJs.exec('mv coverage/badges.svg coverage/cli-badges.svg');
+        if (result.code !== 0) {
+          throw new Error(result.stderr)
+        }
       }
     } catch (e) {
       e.message = e.stack.red

bfbff2e7b939414568fba1e3cfcdb8fe64cbeac8
Riccardo Rotta <riccardorotta@gmail.com>
2023-02-13 10:23:10 +0100

    test: team service tests

--- packages/core/src/services/team-service.spec.ts
+++ packages/core/src/services/team-service.spec.ts
@@ -20,7 +20,21 @@ describe("TeamService", () => {
 
   let teamService;
 
-  beforeEach(async () => {
+  const createTeamServiceInstance = () => {
+    sessionFactory = {};
+    namedProfileService = {};
+    sessionManagementService = {};
+    awsSsoIntegrationService = {};
+    azureIntegrationService = {};
+    idpUrlService = {};
+    keyChainService = {};
+    nativeService = {};
+    fileService = {};
+    repository = {};
+    workspaceService = {};
+    integrationFactory = {};
+    crypto = {};
+    behaviouralSubjectService = {};
     nativeService = {
       os: {
         homedir: () => "",
@@ -42,9 +56,10 @@ describe("TeamService", () => {
       integrationFactory,
       behaviouralSubjectService
     );
-  });
+  };
 
   test("checkSignedInUser() - token not expired yet", async () => {
+    createTeamServiceInstance();
     const mockedKeychainString = `{"key":"value"}`;
     teamService.keyChainService = {
       getSecret: jest.fn(async () => mockedKeychainString),
@@ -64,6 +79,7 @@ describe("TeamService", () => {
   });
 
   test("checkSignedInUser() - token expired", async () => {
+    createTeamServiceInstance();
     const mockedKeychainString = `{"key":"value"}`;
     teamService.keyChainService = {
       getSecret: async () => mockedKeychainString,
@@ -81,6 +97,7 @@ describe("TeamService", () => {
   });
 
   test("checkSignedInUser() - user not present in the keychain", async () => {
+    createTeamServiceInstance();
     const mockedKeychainString = null;
     teamService.keyChainService = {
       getSecret: async () => mockedKeychainString,
@@ -94,5 +111,64 @@ describe("TeamService", () => {
 
     const result = await teamService.checkSignedInUser();
     expect(result).toEqual(false);
+  });
+
+  test("signIn()", async () => {
+    createTeamServiceInstance();
+    teamService.userProvider = {
+      signIn: jest.fn(),
+    };
+    teamService.setSignedInUser = jest.fn();
+
+    await teamService.signIn("mock-email", "mock-password");
+    expect(teamService.userProvider.signIn).toHaveBeenCalledWith("mock-email", "mock-password");
+    expect(teamService.setSignedInUser).toHaveBeenCalled();
+  });
+
+  describe("setSignedInUser()", () => {
+    beforeEach(() => {
+      createTeamServiceInstance();
+      teamService.keyChainService = {
+        saveSecret: jest.fn(),
+        deleteSecret: jest.fn(),
+      };
+      teamService.signedInUser$ = {
+        next: jest.fn(),
+      };
+    });
+
+    test("setSignedInUser() - adding the user to the keychain", async () => {
+      const mockUser: any = {
+        user: "mock-user",
+      };
+      await teamService.setSignedInUser(mockUser);
+      expect(teamService.keyChainService.saveSecret).toHaveBeenCalledWith(constants.appName, "team-signed-in-user", JSON.stringify(mockUser));
+      expect(teamService.keyChainService.deleteSecret).not.toHaveBeenCalled();
+      expect(teamService.signedInUser$.next).toHaveBeenCalledWith(mockUser);
+    });
+
+    test("setSignedInUser() - removing the user to the keychain", async () => {
+      const mockUser = undefined;
+      await teamService.setSignedInUser(mockUser);
+      expect(teamService.keyChainService.saveSecret).not.toHaveBeenCalled();
+      expect(teamService.keyChainService.deleteSecret).toHaveBeenCalledWith(constants.appName, "team-signed-in-user");
+      expect(teamService.signedInUser$.next).toHaveBeenCalledWith(mockUser);
+    });
+  });
+
+  describe("TeamService.signOut()", () => {
+    beforeEach(() => {
+      createTeamServiceInstance();
+      teamService.behaviouralSubjectService = {
+        sessions: [],
+        reloadSessionsAndIntegrationsFromRepository: jest.fn(),
+      };
+      teamService.sessionFactory = {
+        getSessionService: jest.fn(),
+      };
+      teamService.integrationFactory = {
+        getIntegrationService: jest.fn(),
+      };
+    });
   });
 });

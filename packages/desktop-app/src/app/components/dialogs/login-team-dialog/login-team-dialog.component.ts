e1d3f3b33fa67dcc067d0b3eff1d83cffbfafc5f
Riccardo Rotta <riccardorotta@gmail.com>
2023-04-20 16:06:00 +0200

    chore: created and styled login team dialog

--- packages/desktop-app/src/app/components/dialogs/login-team-dialog/login-team-dialog.component.ts
+++ packages/desktop-app/src/app/components/dialogs/login-team-dialog/login-team-dialog.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from "@angular/core";
 import { AppService } from "../../../services/app.service";
 import { AppProviderService } from "../../../services/app-provider.service";
 import { LoggedEntry, LogLevel, LogService } from "@noovolari/leapp-core/services/log-service";
-import { User } from "leapp-team-core/user/user";
-import { MessageToasterService, ToastLevel } from "../../../services/message-toaster.service";
 import { TeamService } from "@noovolari/leapp-core/services/team-service";
-import { BsModalRef } from "ngx-bootstrap/modal";
+import { AbstractControl, FormControl, FormGroup, Validators } from "@angular/forms";
+import { ApiErrorCodes } from "leapp-team-core/errors/api-error-codes";
+import { FormErrorCodes } from "leapp-team-core/errors/form-error-codes";
 
 @Component({
   selector: "app-login-team-dialog",
@@ -13,32 +13,65 @@ import { BsModalRef } from "ngx-bootstrap/modal";
   styleUrls: ["./login-team-dialog.component.scss"],
 })
 export class LoginTeamDialogComponent implements OnInit {
+  email: FormControl;
+  password: FormControl;
+  signinForm: FormGroup;
+  hidePassword?: boolean;
+  submitting?: boolean;
+
   private loggingService: LogService;
   private teamService: TeamService;
 
-  constructor(
-    private bsModalRef: BsModalRef,
-    public appService: AppService,
-    public appProviderService: AppProviderService,
-    private messageToasterService: MessageToasterService
-  ) {
+  constructor(public appService: AppService, public appProviderService: AppProviderService) {
+    this.email = new FormControl("", [Validators.required, Validators.email]);
+    this.password = new FormControl("", [Validators.required]);
+    this.signinForm = new FormGroup({ email: this.email, password: this.password });
+    this.hidePassword = true;
     this.loggingService = appProviderService.logService;
     this.teamService = appProviderService.teamService;
   }
 
+  async signIn(): Promise<void> {
+    this.signinForm.markAllAsTouched();
+
+    if (this.signinForm.valid) {
+      this.submitting = true;
+      const formValue = this.signinForm.value;
+      try {
+        await this.teamService.signIn(formValue.email, formValue.password);
+        this.loggingService.log(new LoggedEntry(`Welcome ${formValue.email}!`, this, LogLevel.success, true));
+        this.closeModal();
+      } catch (responseException: any) {
+        if (responseException.error?.errorCode === ApiErrorCodes.invalidCredentials) {
+          this.password.setErrors({ [FormErrorCodes.invalidCredentials]: {} });
+        } else if (responseException.error?.errorCode === ApiErrorCodes.userNotActive) {
+          this.loggingService.log(new LoggedEntry("The user is not active", this, LogLevel.error, true));
+        } else {
+          this.loggingService.log(new LoggedEntry(responseException, this, LogLevel.error, true));
+        }
+      } finally {
+        this.submitting = false;
+      }
+    }
+  }
+
+  getFormError(control: AbstractControl): string {
+    if (control.errors?.required) {
+      return "Field is required";
+    }
+    if (control.errors?.email) {
+      return "Invalid email";
+    }
+    if (control.errors?.[FormErrorCodes.invalidCredentials]) {
+      return "Invalid email or password";
+    }
+    if (control.errors) {
+      return "Unknown error";
+    }
+    return "";
+  }
+
   ngOnInit(): void {}
-
-  async onSignIn(user: User): Promise<void> {
-    await this.teamService.setSignedInUser(user);
-    this.closeModal();
-    this.loggingService.log(new LoggedEntry(`Welcome ${user.firstName}`, this, LogLevel.success));
-    this.messageToasterService.toast(`Welcome ${user.firstName}`, ToastLevel.success, "Log In to Team Portal");
-  }
-
-  async onError(error: string | number): Promise<void> {
-    this.loggingService.log(new LoggedEntry(`Error while trying to sign in: ${error}`, this, LogLevel.warn));
-    this.messageToasterService.toast(`Error while trying to sign in: ${error}`, ToastLevel.warn, "Log In to Team Portal");
-  }
 
   goBack(): void {
     this.appService.closeModal();

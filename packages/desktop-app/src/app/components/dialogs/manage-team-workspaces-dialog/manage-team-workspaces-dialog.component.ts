4f166f4d10fd26ae7c016cbfcaa17a035c7ea3a0
Eric Villa <eric.villa91@gmail.com>
2023-04-27 16:07:32 +0200

    chore: bound sync and sign-out logic to the manage workspaces buttons

--- packages/desktop-app/src/app/components/dialogs/manage-team-workspaces-dialog/manage-team-workspaces-dialog.component.ts
+++ packages/desktop-app/src/app/components/dialogs/manage-team-workspaces-dialog/manage-team-workspaces-dialog.component.ts
@@ -4,6 +4,11 @@ import { constants } from "@noovolari/leapp-core/models/constants";
 import { AppProviderService } from "../../../services/app-provider.service";
 import { BehaviouralSubjectService } from "@noovolari/leapp-core/services/behavioural-subject-service";
 import { User } from "leapp-team-core/user/user";
+import { LoggedEntry, LogLevel } from "@noovolari/leapp-core/services/log-service";
+import { LoginTeamDialogComponent } from "../login-team-dialog/login-team-dialog.component";
+import { BsModalService } from "ngx-bootstrap/modal";
+import { globalFilteredSessions, globalHasFilter, globalResetFilter } from "../../command-bar/command-bar.component";
+import { sidebarHighlight } from "../../side-bar/side-bar.component";
 
 @Component({
   selector: "app-manage-team-workspaces-dialog",
@@ -17,7 +22,7 @@ export class ManageTeamWorkspacesDialogComponent implements OnInit {
   private behaviouralSubjectService: BehaviouralSubjectService;
   private userSubscription;
 
-  constructor(private appProviderService: AppProviderService, public appService: AppService) {
+  constructor(private appProviderService: AppProviderService, public appService: AppService, private bsModalService: BsModalService) {
     this.behaviouralSubjectService = appProviderService.behaviouralSubjectService;
     this.loggedUser = null;
     this.localWorkspaceName = constants.localWorkspaceName;
@@ -27,11 +32,59 @@ export class ManageTeamWorkspacesDialogComponent implements OnInit {
     return !!this.loggedUser;
   }
 
+  get isTeamLocked(): boolean {
+    return !this.loggedUser?.accessToken;
+  }
+
   ngOnInit(): void {
     this.userSubscription = this.appProviderService.teamService.signedInUserState.subscribe((user: User) => (this.loggedUser = user));
   }
 
   closeModal(): void {
     this.appService.closeModal();
+  }
+
+  async syncWorkspace(): Promise<void> {
+    try {
+      this.closeModal();
+      //await this.appProviderService.teamService.syncSecrets();
+      await this.switchToRemoteWorkspace();
+    } catch (error) {
+      this.appProviderService.logService.log(new LoggedEntry(error.message, this, LogLevel.error, true));
+    }
+  }
+
+  async signOutFromWorkspace(): Promise<void> {
+    try {
+      await this.appProviderService.teamService.signOut();
+    } catch (error) {
+      this.appProviderService.logService.log(new LoggedEntry(error.message, this, LogLevel.error, true));
+    }
+  }
+
+  async switchToRemoteWorkspace(): Promise<void> {
+    if (this.isTeamLocked) {
+      await this.loginToLeappTeam();
+    } else {
+      await this.appProviderService.teamService.syncSecrets();
+      this.resetFilters();
+    }
+  }
+
+  async loginToLeappTeam(): Promise<void> {
+    this.bsModalService.show(LoginTeamDialogComponent, {
+      animated: false,
+      class: "create-modal",
+      backdrop: "static",
+      keyboard: false,
+    });
+  }
+
+  resetFilters(): void {
+    document.querySelector(".sessions").classList.remove("filtered");
+    sidebarHighlight.next({ showAll: true, showPinned: false, selectedSegment: -1 });
+    globalFilteredSessions.next(this.behaviouralSubjectService.sessions);
+    globalHasFilter.next(false);
+    globalResetFilter.next(true);
   }
 }

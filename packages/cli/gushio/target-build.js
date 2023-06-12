c083c0c2404d5945e68f5bd8f21bc5d8fc7a753a
Marco Vanetti <marco.vanetti@gmail.com>
2023-06-12 12:01:33 +0200

    chore: added team-service-stub support to the cli build script

--- packages/cli/gushio/target-build.js
+++ packages/cli/gushio/target-build.js
@@ -7,12 +7,19 @@ module.exports = {
   run: async () => {
     const path = require('path')
     const shellJs = require('shelljs')
+    const fs = require("fs");
     const compileFunction = require('./compile-func')
 
     try {
       await gushio.run(path.join(__dirname, './target-clean.js'))
-
       console.log('Building leapp CLI... ')
+
+      const teamServiceStubFile = path.join(__dirname, '../src/service/team-service-stub.ts')
+      const teamServiceTargetFile = path.join(__dirname, '../src/service/team-service.ts')
+      if (!fs.existsSync(teamServiceTargetFile)) {
+        fs.copyFileSync(teamServiceStubFile, teamServiceTargetFile)
+      }
+
       await compileFunction(path, shellJs)
 
       console.log('Build completed successfully')

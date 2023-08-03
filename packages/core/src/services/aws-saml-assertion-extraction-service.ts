73f6044cd0f79bcd376072cd907dd68487400839
Riccardo Rotta <riccardorotta@gmail.com>
2023-08-03 11:33:37 +0200

    fix: fixed SAML authentication timeout exceeded error

--- packages/core/src/services/aws-saml-assertion-extraction-service.ts
+++ packages/core/src/services/aws-saml-assertion-extraction-service.ts
@@ -17,6 +17,7 @@ const authenticationUrlRegexes = new Map([
       /^https:\/\/.+\.auth0\.com\/samlp\/.+/,
       /^https:\/\/.*\/auth\/realms\/.*\/protocol\/saml\/clients\/.*/,
       /^https:\/\/console\.jumpcloud\.com\/login.*/,
+      /^https:\/\/accounts\.google\.com\/AccountChooser.*/,
     ],
   ],
 ]);

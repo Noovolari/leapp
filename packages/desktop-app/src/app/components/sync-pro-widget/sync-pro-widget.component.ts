5bf341a1acf3e2c8de66501ef11e8727f541de3c
Alessandro Gaggia <alex@besharp.it>
2023-08-31 15:54:18 +0200

    chore: fixed information for plans in the plan page

--- packages/desktop-app/src/app/components/sync-pro-widget/sync-pro-widget.component.ts
+++ packages/desktop-app/src/app/components/sync-pro-widget/sync-pro-widget.component.ts
@@ -2,9 +2,11 @@ import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Subscription } from "rxjs";
 import { AppProviderService } from "../../services/app-provider.service";
 import { BsModalService } from "ngx-bootstrap/modal";
-import { LeappPlanStatus, OptionsDialogComponent } from "../dialogs/options-dialog/options-dialog.component";
+import { globalLeappProPlanStatus, LeappPlanStatus, OptionsDialogComponent } from "../dialogs/options-dialog/options-dialog.component";
 import { WorkspaceState } from "../../services/team-service";
 import { Role } from "../../leapp-team-core/user/role";
+import { Router } from "@angular/router";
+import { LoginWorkspaceDialogComponent } from "../dialogs/login-team-dialog/login-workspace-dialog.component";
 
 @Component({
   selector: "app-sync-pro-widget",
@@ -15,11 +17,12 @@ export class SyncProWidgetComponent implements OnInit, OnDestroy {
   isLoggedAsPro = false;
   isFailed = false;
   isProgress = false;
-  canRemoveButton = false;
+  buttonText = "UPGRADE TO PRO";
 
   private subscription: Subscription;
+  private subscription2: Subscription;
 
-  constructor(private appProviderService: AppProviderService, private bsModalService: BsModalService) {}
+  constructor(private appProviderService: AppProviderService, private bsModalService: BsModalService, private router: Router) {}
 
   ngOnInit(): void {
     this.isLoggedAsPro = false;
@@ -28,17 +31,35 @@ export class SyncProWidgetComponent implements OnInit, OnDestroy {
       this.isLoggedAsPro = !!workspaceState;
       this.isProgress = workspaceState?.syncState === "in-progress";
       this.isFailed = workspaceState?.syncState === "failed";
-      const currentState = await this.appProviderService.keychainService.getSecret("Leapp", "leapp-enabled-plan");
-      this.canRemoveButton = currentState === LeappPlanStatus.proPending.toString() || currentState === LeappPlanStatus.proEnabled.toString();
+      this.subscription2 = globalLeappProPlanStatus.subscribe(async (_) => {
+        const currentState = await this.appProviderService.keychainService.getSecret("Leapp", "leapp-enabled-plan");
+        if (currentState === LeappPlanStatus.proEnabled || currentState === LeappPlanStatus.proPending) {
+          this.buttonText = "SIGN IN";
+        } else {
+          this.buttonText = "UPGRADE TO PRO";
+        }
+      });
     });
   }
 
   ngOnDestroy(): void {
     this.subscription?.unsubscribe();
+    this.subscription2?.unsubscribe();
   }
 
-  goToUpgrade(): void {
-    this.bsModalService.show(OptionsDialogComponent, { animated: false, class: "option-modal", initialState: { selectedIndex: 6 } });
+  async callToAction(): Promise<void> {
+    if (this.buttonText === "UPGRADE TO PRO") {
+      this.bsModalService.show(OptionsDialogComponent, { animated: false, class: "option-modal", initialState: { selectedIndex: 6 } });
+    } else if (this.buttonText === "SIGN IN") {
+      const isLeappTeamStubbed = this.appProviderService.teamService.isLeappTeamStubbed;
+      if (isLeappTeamStubbed) return;
+      this.bsModalService.show(LoginWorkspaceDialogComponent, {
+        animated: false,
+        class: "create-modal",
+        backdrop: "static",
+        keyboard: false,
+      });
+    }
   }
 
   // TODO: is it still used?

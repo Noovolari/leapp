97c5119bb2ca026830ddcf9513810af2bfeaf8bb
Alessandro Gaggia <alex@besharp.it>
2023-09-14 10:06:18 +0200

    chore: reintroduced sign for mac

--- packages/desktop-app/gushio/target-release-dev.js
+++ packages/desktop-app/gushio/target-release-dev.js
@@ -32,18 +32,13 @@ module.exports = {
 
       shellJs.cd(path.join(__dirname, '..'));
 
-      packageJson["build"]["mac"]["forceCodeSigning"] = false;
+      packageJson["build"]["mac"]["forceCodeSigning"] = true;
       delete packageJson["build"]["afterSign"];
       delete packageJson["build"]["win"]["signingHashAlgorithms"];
       delete packageJson["build"]["win"]["sign"];
       await writePackageJsonFunction(path, "desktop-app", packageJson);
 
-      let command;
-      if (args[1] === 'mac') {
-        command = `export CSC_IDENTITY_AUTO_DISCOVERY=false && electron-builder build ${platformVersion}`;
-      } else {
-        command = `electron-builder build ${platformVersion}`;
-      }
+      let command = `electron-builder build ${platformVersion}`;
       const result = shellJs.exec(command);
       if (result.code !== 0) {
         throw new Error(result.stderr)
